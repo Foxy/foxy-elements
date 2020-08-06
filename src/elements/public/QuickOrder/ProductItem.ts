566e37703749bd96038ec625243f4a60dc2b8067
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-08-05 23:07:15 -0300

    refactor(quickorder): simplify, remove unused code

---
+++ src/elements/public/QuickOrder/ProductItem.ts
@@ -0,0 +1,162 @@
+import { Translatable } from '../../../mixins/translatable';
+import { QuickOrderProduct } from './types';
+import { html, property } from 'lit-element';
+import { Checkbox, Section, Group, I18N } from '../../private/index';
+
+/**
+ * This component allows a user to configure a product.
+ *
+ * The product may be configured using HTML properties or a JS object.
+ * Relevant properties are mapped to a QuickOrderProduct object that is used by
+ * the QuickOrderForm.
+ */
+export class ProductItem extends Translatable {
+  public static get scopedElements() {
+    return {
+      'x-checkbox': Checkbox,
+      'x-section': Section,
+      'x-group': Group,
+      'x-number-field': customElements.get('vaadin-number-field'),
+      'x-i18n': I18N,
+    };
+  }
+
+  private __vocabulary = {
+    remove: 'Remove',
+  };
+
+  public constructor() {
+    super('quick-order');
+  }
+
+  /** LitElement life cicle */
+  public firstUpdated(): void {
+    this.__propertyToValue();
+  }
+
+  private __default_image = {
+    src: 'https://www.foxy.io/merchants/shopping-cart-full.svg',
+    alt: 'A sketch of a shopping cart with three boxes',
+  };
+
+  public value: QuickOrderProduct | undefined;
+
+  @property({ type: String })
+  public name?: string;
+
+  @property({ type: String })
+  public price?: number;
+
+  @property({ type: String })
+  public image?: string;
+
+  @property({ type: String })
+  public url?: string;
+
+  @property({ type: String })
+  public code?: string;
+
+  @property({ type: String })
+  public parent_code?: string;
+
+  @property({ type: String })
+  public quantity?: number;
+
+  @property({ type: String })
+  public quantity_max?: number;
+
+  @property({ type: String })
+  public quantity_min?: number;
+
+  @property({ type: String })
+  public category?: string;
+
+  @property({ type: String })
+  public expires?: string;
+
+  @property({ type: String })
+  public weight?: string;
+
+  @property({ type: String })
+  public length?: number;
+
+  @property({ type: String })
+  public width?: number;
+
+  @property({ type: String })
+  public height?: number;
+
+  @property({ type: String })
+  public shipto?: string;
+
+  @property({ type: String })
+  alt?: string;
+
+  public render() {
+    return html`
+      <article
+        data-product="true"
+        class="product flex flex-row flex-wrap justify-between overflow-hidden"
+      >
+        <img
+          class="max-w-xs min-w-1 block"
+          alt="${this.value?.alt ?? this.__default_image.alt}"
+          src="${this.value?.image ?? this.__default_image.src}"
+        />
+        <x-section class="description flex flex-wrap flex-column p-s min-w-xl">
+          <h1>${this.value?.name}</h1>
+          <div class="product-description">${this.value?.description}</div>
+        </x-section>
+        <x-section class="item-info p-s min-w-2">
+          <div class="price">${this.value?.price}</div>
+        </x-section>
+        <x-section class="actions p-s min-w-3">
+          <x-number-field value="1" min="0" has-controls></x-number-field>
+          <x-checkbox data-testid="toggle">${this.__vocabulary.remove}</x-checkbox>
+        </x-section>
+      </article>
+    `;
+  }
+
+  /** Captures values set as properties to build the value property of the component.  */
+  private __propertyToValue() {
+    if (this.value === undefined) {
+      if (this.name && this.price) {
+        this.value = {
+          name: this.name,
+          price: this.price,
+        };
+      } else {
+        console.error('The name and price attributes of a product are required.', {
+          name: this.name,
+          price: this.price,
+        });
+      }
+    }
+    const productProperties = [
+      'name',
+      'price',
+      'image',
+      'url',
+      'code',
+      'parent_code',
+      'quantity',
+      'quantity_max',
+      'quantity_min',
+      'category',
+      'expires',
+      'weight',
+      'length',
+      'width',
+      'height',
+      'shipto',
+      'id',
+      'alt',
+    ];
+    for (const i of productProperties) {
+      if (!(this.value![i] as string | null)) {
+        this.value![i] = this.getAttribute(i);
+      }
+    }
+  }
+}
--- src/elements/public/QuickOrder/QuickOrder.ts
+++ src/elements/public/QuickOrder/QuickOrder.ts
@@ -1,22 +1,19 @@
 import '@vaadin/vaadin-text-field/vaadin-integer-field';
 import '@vaadin/vaadin-text-field/vaadin-password-field';
-import { html, property } from 'lit-element';
+import { html, property, TemplateResult } from 'lit-element';
 import { Translatable } from '../../../mixins/translatable';
-import { interpret } from 'xstate';
-import { Product } from './private/Product';
+import { ProductItem } from './ProductItem';
 
 import { QuickOrderProduct } from './types';
 
-import { machine } from './machine';
 import { Section, Page, Code, I18N, Skeleton } from '../../private/index';
 
-type CustomWindow = Window & { FC?: any };
 export class QuickOrder extends Translatable {
   public static get scopedElements() {
     return {
       'vaadin-integer-field': customElements.get('vaadin-integer-field'),
       'vaadin-password-field': customElements.get('vaadin-password-field'),
-      'x-product': Product,
+      'x-product': ProductItem,
       'x-skeleton': Skeleton,
       'x-section': Section,
       'x-i18n': I18N,
@@ -32,102 +29,31 @@ export class QuickOrder extends Translatable {
 
   constructor() {
     super('quick-order');
-    this.loadFoxy();
   }
 
-  /** Creates a script tag for loader.js if it not exists and sets a
-   * ready.done callback */
-  private loadFoxy() {
-    if (!('FC' in window)) {
-      // Compute src
-      let storeName = this.storeSubdomain;
-      if (this.storeSubdomain.endsWith('.foxycart.com')) {
-        storeName = this.storeSubdomain.replace('.foxycart.com', '');
-      }
-      const src = `https://cdn.foxycart.com/${storeName}/loader.js`;
-      // Check if script is present
-      const loader = document.querySelector(`script[src="${src}"]`);
-      // Insert loader if not present
-      if (!loader) {
-        const script = document.createElement('script');
-        if (!document.querySelector('foxy-loader-script')) {
-          script.type = 'text/javascript';
-          script.setAttribute('data-cfasync', 'false');
-          script.async = true;
-          script.defer = true;
-          script.setAttribute('id', 'foxy-loader-script');
-          let storeName = this.storeSubdomain;
-          if (this.storeSubdomain.endsWith('.foxycart.com')) {
-            storeName = this.storeSubdomain.replace('.foxycart.com', '');
-          }
-          script.src = src;
-          document.head.appendChild(script);
-        }
-      }
-      const W = window as CustomWindow;
-      W.FC = W.FC || {};
-      // Create FC onload
-      const originalCallback = W.FC.onLoad;
-      W.FC.onLoad = () => {
-        if (originalCallback != undefined) {
-          originalCallback();
-        }
-        W.FC.client.on('ready.done', () => {
-          this.updateFromFC();
-        });
-      };
-    }
-  }
-
-  /** Update the form with values from FC */
-  private updateFromFC() {
-    const FC = (window as CustomWindow).FC;
-  }
+  private __productElements: ProductItem[] = [];
 
   @property({ type: Array })
   products: QuickOrderProduct[] = [];
 
-  @property({ type: Boolean, noAccessor: true })
-  public get disabled() {
-    return this.service.state.matches('disabled');
-  }
-  public set disabled(value: boolean) {
-    this.service.send(value ? 'DISABLE' : 'ENABLE');
-  }
-
-  public service = interpret(machine)
-    .onChange(() => this.requestUpdate())
-    .onTransition(() => this.requestUpdate())
-    .start();
-
-  public render() {
+  public render(): TemplateResult {
     return html`
       <x-page>
-        Produtos ${this.products} ${this.__renderHeader()}
-
         <x-section class="products">
+          <slot></slot>
           ${this.products.map(
-            (p: QuickOrderProduct) => html`<x-product
-              .lang=${this.lang}
-              .disabled=${this.disabled}
-              .value=${p}
-            ></x-product>`
+            (p: QuickOrderProduct) => html` <x-group frame>
+              <x-product .value=${p}></x-product>
+            </x-group>`
           )}
         </x-section>
       </x-page>
     `;
   }
 
-  private __renderHeader() {
-    return html`
-      <x-i18n class="block" slot="title" key="title" .ns=${this.ns} .lang=${this.lang}></x-i18n>
-      <x-i18n
-        class="block"
-        slot="subtitle"
-        key="subtitle"
-        .ns=${this.ns}
-        .lang=${this.lang}
-      ></x-i18n>
-    `;
+  private __findProducts() {
+    this.querySelectorAll('[data-product=true]').forEach(p =>
+      this.__productElements.push(p as ProductItem)
+    );
   }
 }
