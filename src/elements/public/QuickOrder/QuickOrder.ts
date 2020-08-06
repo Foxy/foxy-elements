ae7cd13ec2cc980f63511718371c26f935f674f0
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-08-06 10:10:49 -0300

    feat(quickorder): group products in array

--- src/elements/public/QuickOrder/QuickOrder.ts
+++ src/elements/public/QuickOrder/QuickOrder.ts
@@ -4,7 +4,7 @@ import { html, property, TemplateResult } from 'lit-element';
 import { Translatable } from '../../../mixins/translatable';
 import { ProductItem } from './ProductItem';
 
-import { QuickOrderProduct } from './types';
+import { QuickOrderProduct, ProductGroup } from './types';
 
 import { Section, Page, Code, I18N, Skeleton } from '../../private/index';
 
@@ -41,14 +41,24 @@ export class QuickOrder extends Translatable {
       <x-page>
         <x-section class="products">
           <slot></slot>
-          ${this.products.map(
-            (p: QuickOrderProduct) => html` <x-group frame>
-              <x-product .value=${p}></x-product>
-            </x-group>`
-          )}
+          ${this.products.map(this.__productsFromArray)}
         </x-section>
       </x-page>
     `;
+  }
+
+  /** Renders either a group or a single product */
+  private __productsFromArray(p: QuickOrderProduct | ProductGroup) {
+    if (p.products && Array.isArray(p.products) && p.products.length) {
+      return html` <article
+        data-product="true"
+        class="product group  border border-contrast-10 rounded p-m"
+      >
+        ${p.products.map((i: QuickOrderProduct) => html`<x-product .value=${i}></x-product>`)}
+      </article>`;
+    } else {
+      return html`<x-product .value=${p}></x-product>`;
+    }
   }
 
   private __findProducts() {
--- src/elements/public/QuickOrder/types.ts
+++ src/elements/public/QuickOrder/types.ts
@@ -19,3 +19,21 @@ export interface QuickOrderProduct {
   alt?: string;
   [propName: string]: string | number | null | undefined;
 }
+
+export interface ProductGroup {
+  products: QuickOrderProduct[];
+  sub_frequency?: string;
+  'data-sub-frequency'?: string;
+  sub_startdate?: string;
+  'data-sub-startdate'?: string;
+  sub_enddate?: string;
+  'data-sub-enddate'?: string;
+  sub_token?: string;
+  'data-sub-token'?: string;
+  sub_cancel?: string;
+  'data-sub-cancel'?: string;
+  sub_restart?: string;
+  'data-sub-restart'?: string;
+  sub_modify?: string;
+  'data-sub-modify'?: string;
+}
