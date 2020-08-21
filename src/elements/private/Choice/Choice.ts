fecfcd479f05c90c476e384aee59c752626f21e2
Daniil Bratukhin <dantothefuture@gmail.com>
2020-08-22 00:35:36 +0300

    feat(choice): add support for custom values

--- src/elements/private/Choice/Choice.ts
+++ src/elements/private/Choice/Choice.ts
@@ -1,19 +1,64 @@
 import { spread } from '@open-wc/lit-helpers/src/spread';
 import { ScopedElementsMap } from '@open-wc/scoped-elements';
-import '@vaadin/vaadin-checkbox/vaadin-checkbox';
-import '@vaadin/vaadin-checkbox/vaadin-checkbox-group';
-import '@vaadin/vaadin-radio-button/vaadin-radio-group';
-import { css, CSSResultArray, html, property, TemplateResult } from 'lit-element';
-import { Themeable } from '../../../mixins/themeable';
+import '@vaadin/vaadin-text-field/vaadin-integer-field';
+import '@vaadin/vaadin-text-field/vaadin-text-area';
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import '@vaadin/vaadin-lumo-styles/icons';
+import '@polymer/iron-icon';
+import { css, CSSResultArray, html, internalProperty, property, TemplateResult } from 'lit-element';
+import { Translatable } from '../../../mixins/translatable';
 import { ChoiceChangeEvent } from './ChoiceChangeEvent';
+import { AttributePart } from 'lit-html';
 
-export class Choice extends Themeable {
+const VALUE_OTHER = `@foxy.io/elements::other[${(Math.pow(10, 10) * Math.random()).toFixed(0)}]`;
+
+function radio(checked: boolean, attrs: (part: AttributePart) => void, label: TemplateResult) {
+  const ease = 'transition ease-in-out duration-200';
+  const box = `${ease} ${checked ? 'bg-primary' : 'bg-contrast-20 group-hover:bg-contrast-30'}`;
+  const dot = `${ease} transform ${checked ? 'scale-100' : 'scale-0'}`;
+
+  return html`
+    <label class="group flex items-center cursor-pointer">
+      <div class="item flex items-center justify-center">
+        <div class="flex radio rounded-full ${box} focus-within:shadow-outline">
+          <div class="dot m-auto rounded-full bg-tint shadow-xs ${dot}"></div>
+          <input type="radio" class="sr-only" ?checked=${checked} ...=${attrs} />
+        </div>
+      </div>
+      <div class="font-lumo text-body leading-m">
+        ${label}
+      </div>
+    </label>
+  `;
+}
+
+function check(checked: boolean, attrs: (part: AttributePart) => void, label: TemplateResult) {
+  const ease = 'transition ease-in-out duration-200';
+  const box = `${ease} ${checked ? 'bg-primary' : 'bg-contrast-20 group-hover:bg-contrast-30'}`;
+  const dot = `${ease} transform ${checked ? 'scale-100' : 'scale-0'}`;
+
+  return html`
+    <label class="group flex items-center cursor-pointer">
+      <div class="item flex items-center justify-center text-primary-contrast">
+        <div class="check rounded-s ${box} focus-within:shadow-outline">
+          <iron-icon icon="lumo:checkmark" class="block w-full h-full ${dot}"></iron-icon>
+          <input type="checkbox" class="sr-only" ?checked=${checked} ...=${attrs} />
+        </div>
+      </div>
+      <div class="font-lumo text-body leading-m">
+        ${label}
+      </div>
+    </label>
+  `;
+}
+
+export class Choice extends Translatable {
   public static get scopedElements(): ScopedElementsMap {
     return {
-      'vaadin-checkbox-group': customElements.get('vaadin-checkbox-group'),
-      'vaadin-checkbox': customElements.get('vaadin-checkbox'),
-      'vaadin-radio-button': customElements.get('vaadin-radio-button'),
-      'vaadin-radio-group': customElements.get('vaadin-radio-group'),
+      'vaadin-integer-field': customElements.get('vaadin-integer-field'),
+      'vaadin-text-field': customElements.get('vaadin-text-field'),
+      'vaadin-text-area': customElements.get('vaadin-text-area'),
+      'iron-icon': customElements.get('iron-icon'),
     };
   }
 
@@ -21,15 +66,90 @@ export class Choice extends Themeable {
     return [
       super.styles,
       css`
+        :host {
+          --item-width: calc((var(--lumo-space-m) * 2) + 1.25rem);
+        }
+
         .ml-xxl {
-          margin-left: 3rem;
+          margin-left: var(--item-width);
+        }
+
+        .item {
+          height: var(--lumo-size-l);
+          width: var(--item-width);
+        }
+
+        .radio {
+          height: 1.25rem;
+          width: 1.25rem;
+        }
+
+        .check {
+          height: 1.125rem;
+          width: 1.125rem;
+        }
+
+        .dot {
+          height: 0.5rem;
+          width: 0.5rem;
         }
       `,
     ];
   }
 
+  private get __field() {
+    const handleInput = (evt: Event) => {
+      evt.stopPropagation();
+      const customValue = (evt.target as HTMLInputElement).value;
+
+      if (Array.isArray(this.value)) {
+        this.value = this.value.slice(0, this.value.length - 1).concat(customValue);
+      } else {
+        this.value = customValue;
+      }
+
+      this.dispatchEvent(new ChoiceChangeEvent(this.value));
+    };
+
+    const attributes = spread({
+      placeholder: this._t('choice.other').toString(),
+      class: 'w-full mb-m',
+      value: this.__customValue,
+      max: this.max,
+      min: this.min,
+      '@input': handleInput,
+      '@change': handleInput,
+    });
+
+    if (this.type === 'integer') {
+      return html`<vaadin-integer-field ...=${attributes} has-controls></vaadin-integer-field>`;
+    } else if (this.type === 'textarea') {
+      return html`<vaadin-text-area ...=${attributes}></vaadin-text-area>`;
+    } else {
+      return html`<vaadin-text-field ...=${attributes}></vaadin-text-field>`;
+    }
+  }
+
+  @internalProperty()
+  private __customValue = '';
+
+  @internalProperty()
+  private __customValueVisible = false;
+
   @property({ type: Boolean })
   public disabled = false;
+
+  @property({ type: Boolean })
+  public custom = false;
+
+  @property({ type: String })
+  public type: 'text' | 'textarea' | 'integer' = 'text';
+
+  @property({ type: Number })
+  public min: number | null = null;
+
+  @property({ type: Number })
+  public max: number | null = null;
 
   @property({ type: Array })
   public value: null | string | string[] = null;
@@ -41,25 +161,48 @@ export class Choice extends Themeable {
   public getText: (value: string) => string = v => v;
 
   public render(): TemplateResult {
+    const items = this.custom ? [...this.items, VALUE_OTHER] : this.items;
     const multiple = Array.isArray(this.value);
 
     const children = html`
-      ${this.items.map((item, index) => {
+      ${items.map((item, index, array) => {
+        const other = this.custom && index === array.length - 1;
+
+        const checked = other
+          ? this.__customValueVisible
+          : multiple
+          ? !!this.value?.includes(item)
+          : !this.__customValueVisible && item === String(this.value);
+
         const attributes = spread({
-          class: 'w-full',
-          value: item,
-          style: 'margin-left: 0.8125rem',
+          value: other ? VALUE_OTHER : item,
+          name: multiple ? item : 'choice',
           'data-testid': `item-${item}`,
-          '?checked': multiple ? this.value?.includes(item) : item === this.value,
-          '@keydown': multiple ? null : this.__overrideFocus,
+          '?checked': checked,
           '@change': (evt: Event) => {
             const checked = (evt.target as HTMLInputElement).checked;
+
+            if (multiple) {
+              if (item === VALUE_OTHER) this.__customValueVisible = checked;
+            } else {
+              this.__customValueVisible = checked && item === VALUE_OTHER;
+            }
+
+            if (item === VALUE_OTHER) {
+              this.__customValue = multiple ? '' : (this.value as string);
+            }
+
+            const newItem = item === VALUE_OTHER ? this.__customValue : item;
             const value = this.value;
 
             if (Array.isArray(value)) {
-              this.value = checked ? [...value, item] : value.filter(v => v !== item);
+              if (item === VALUE_OTHER) {
+                this.value = checked ? [...value, newItem] : value.slice(0, value.length - 1);
+              } else {
+                this.value = checked ? [newItem, ...value] : value.filter(v => v !== newItem);
+              }
             } else {
-              this.value = checked ? item : null;
+              this.value = checked ? newItem : null;
             }
 
             this.dispatchEvent(new ChoiceChangeEvent(this.value));
@@ -67,53 +210,26 @@ export class Choice extends Themeable {
         });
 
         const label = html`
-          <div style="margin-left: 0.3125rem">
-            <slot name=${`${item}-label`}>${this.getText(item)}</slot>
+          <div>
+            ${item === VALUE_OTHER
+              ? this._t('choice.other').toString()
+              : html`<slot name=${`${item}-label`}>${this.getText(item)}</slot>`}
           </div>
         `;
 
         return html`
-          <div class="my-s ml-xxl border-t border-contrast-10 ${index ? '' : 'hidden'}"></div>
+          <div class="ml-xxl border-t border-contrast-10 ${index ? '' : 'hidden'}"></div>
 
-          ${multiple
-            ? html`<vaadin-checkbox ...=${attributes}>${label}</vaadin-checkbox>`
-            : html`<vaadin-radio-button ...=${attributes}>${label}</vaadin-radio-button>`}
+          ${multiple ? check(checked, attributes, label) : radio(checked, attributes, label)}
 
-          <div class="mr-m mt-s ml-xxl">
-            <slot name=${item}></slot>
+          <div class="mr-m ml-xxl">
+            ${item === VALUE_OTHER && this.__customValueVisible ? this.__field : ''}
+            ${item !== VALUE_OTHER ? html`<slot name=${item}></slot>` : ''}
           </div>
-
-          <div class="mt-s"></div>
         `;
       })}
     `;
 
-    const className = 'pt-s pb-0 pr-0 w-full font-lumo text-body';
-
-    return multiple
-      ? html`<vaadin-checkbox-group class=${className}>${children}</vaadin-checkbox-group>`
-      : html`<vaadin-radio-group class=${className}>${children}</vaadin-radio-group>`;
-  }
-
-  private __overrideFocus(evt: KeyboardEvent) {
-    if (!evt.key.startsWith('Arrow')) return;
-
-    let target = evt.target as HTMLInputElement | null;
-    const targetTagName = target!.tagName;
-
-    evt.preventDefault();
-    evt.stopImmediatePropagation();
-
-    while (target) {
-      target = (evt.key === 'ArrowUp' || evt.key === 'ArrowLeft'
-        ? target.previousElementSibling
-        : target.nextElementSibling) as HTMLInputElement | null;
-
-      if (target?.tagName === targetTagName) {
-        target.click();
-        target.focus();
-        break;
-      }
-    }
+    return html`<form>${children}</form> `;
   }
 }
