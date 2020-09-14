0bf54d1fd0bfc9b1114719f40537ccee1658f694
Daniil Bratukhin <dantothefuture@gmail.com>
2020-09-14 19:12:47 +0300

    build(storybook): fix history state persistence issue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,23 @@
 /* global window */
 
 import { configure, setCustomElements } from '@storybook/web-components';
+import { persistHistoryStateBetweenReloads } from './utils';
 import customElements from '../custom-elements.json';
 
-const context = require.context('../src/elements/public', true, /\.stories\.(js|mdx)$/);
+const context = require.context('../src/elements/public', true, /\.stories\.mdx$/);
 
 setCustomElements(customElements);
 configure(context, module);
 
 if (module.hot) {
-  module.hot.accept(context.id, () => {
-    const currentLocationHref = window.location.href;
-    window.history.pushState(null, null, currentLocationHref);
-    window.location.reload();
+  persistHistoryStateBetweenReloads({
+    lastStoryKey: '@foxy.io/elements::storybook.last_story',
+    lastPathKey: '@foxy.io/elements::storybook.last_path',
+    refreshRate: 250,
+    module,
   });
+
+  module.hot.accept(context.id, () => location.reload());
 }
 
 export const parameters = {
