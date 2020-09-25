2fd0ea3ce1f4f257a332c5a26aa9f67023ac067c
Daniil Bratukhin <dantothefuture@gmail.com>
2020-09-25 23:37:17 +0300

    test: add tests for switch

---
+++ src/elements/private/Switch/Switch.test.ts
@@ -0,0 +1,70 @@
+import { expect, fixture } from '@open-wc/testing';
+import { createModel } from '@xstate/test';
+import { cloneDeep } from 'lodash-es';
+import { Machine } from 'xstate/dist/xstate.web.js';
+import { Switch } from './Switch';
+import { CheckboxMachine } from '../Checkbox/CheckboxMachine';
+
+function getInput(element: Switch) {
+  return element.shadowRoot!.querySelector('input')!;
+}
+
+function testChecked(element: Switch, value: boolean) {
+  expect(getInput(element).checked).to.equal(value);
+}
+
+function testDisabled(element: Switch, value: boolean) {
+  expect(getInput(element).disabled).to.equal(value);
+}
+
+function createSwitchModel() {
+  const config = cloneDeep(CheckboxMachine.config);
+
+  config.states!.checked.meta = {
+    test: (element: Switch) => testChecked(element, true),
+  };
+
+  config.states!.checked.states!.enabled.meta = {
+    test: (element: Switch) => testDisabled(element, false),
+  };
+
+  config.states!.checked.states!.disabled.meta = {
+    test: (element: Switch) => testDisabled(element, true),
+  };
+
+  config.states!.unchecked.meta = {
+    test: (element: Switch) => testChecked(element, false),
+  };
+
+  config.states!.unchecked.states!.enabled.meta = {
+    test: (element: Switch) => testDisabled(element, false),
+  };
+
+  config.states!.unchecked.states!.disabled.meta = {
+    test: (element: Switch) => testDisabled(element, true),
+  };
+
+  return createModel<Switch>(Machine(config)).withEvents({
+    DISABLE: { exec: element => void (element.disabled = true) },
+    ENABLE: { exec: element => void (element.disabled = false) },
+    TOGGLE: { exec: element => getInput(element).click() },
+    FORCE_TOGGLE: { exec: element => void (element.checked = !element.checked) },
+  });
+}
+
+const model = createSwitchModel();
+customElements.define('x-switch', Switch);
+
+describe('Switch', () => {
+  model.getSimplePathPlans().forEach(plan => {
+    describe(plan.description, () => {
+      plan.paths.forEach(path => {
+        it(path.description, async () => path.test(await fixture('<x-switch></x-switch>')));
+      });
+    });
+  });
+
+  describe('has full coverage', () => {
+    it('yes', () => model.testCoverage());
+  });
+});
