7b6f534945e1c5411dbac6aa353ce7786fbca192
Daniil Bratukhin <dantothefuture@gmail.com>
2020-09-28 16:23:33 +0300

    fix(customerportalsettings): reduce hint text size

--- src/elements/public/CustomerPortalSettings/private/NextDateModification/NextDateModification.ts
+++ src/elements/public/CustomerPortalSettings/private/NextDateModification/NextDateModification.ts
@@ -105,10 +105,10 @@ export class NextDateModification extends Translatable {
             .ns=${this.ns}
             key="ndmod.add_hint"
             class=${classMap({
-              'text-s text-center block font-lumo mt-xs transition duration-200 sm:mt-0 sm:ml-m': true,
+              'text-xs text-center block font-lumo mt-xs transition duration-200 sm:mt-0 sm:ml-m': true,
               'text-tertiary': Array.isArray(this.value) && this.value.length < 10,
               'text-primary': Array.isArray(this.value) && this.value.length >= 10,
-              'opacity-0': !Array.isArray(this.value) || this.value.length === 0,
+              hidden: !Array.isArray(this.value) || this.value.length === 0,
             })}
           >
           </x-i18n>
