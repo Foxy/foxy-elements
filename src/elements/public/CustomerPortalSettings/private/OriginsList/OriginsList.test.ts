633c7d70ca3e17c0e84e7f8ddb96aa29a4215914
Daniil Bratukhin <dantothefuture@gmail.com>
2020-10-08 13:47:47 +0300

    test(originslist): add tests for new features

--- src/elements/public/CustomerPortalSettings/private/OriginsList/OriginsList.test.ts
+++ src/elements/public/CustomerPortalSettings/private/OriginsList/OriginsList.test.ts
@@ -1,144 +1,159 @@
 import { expect, fixture } from '@open-wc/testing';
+import { ButtonElement } from '@vaadin/vaadin-button';
+import { TextFieldElement } from '@vaadin/vaadin-text-field';
 import { createModel } from '@xstate/test';
-import { createMachine } from 'xstate';
-import { OriginsList } from './OriginsList';
-import { List } from '../../../../private/index';
+import { createMachine, EventObject } from 'xstate';
+import { getRefs } from '../../../../../utils/test-utils';
 import { ListChangeEvent } from '../../../../private/events';
-import { OriginsListChangeEvent } from './OriginsListChangeEvent';
+import { List } from '../../../../private/index';
+import { OriginsList } from './OriginsList';
 
-customElements.define('x-origins-list', OriginsList);
+class TestOriginsList extends OriginsList {
+  get whenReady() {
+    return this._whenI18nReady.then(() => this.updateComplete);
+  }
+}
 
-const samples = {
-  value: ['https://foxy.io', 'https://foxy-demo.foxycart.com', 'http://localhost:8080'],
-};
+customElements.define('x-origins-list', TestOriginsList);
 
-function getRefs(element: OriginsList) {
-  const $ = (selector: string) => element.shadowRoot!.querySelector(selector);
+interface Refs {
+  list: List;
+  input: TextFieldElement;
+  button: ButtonElement;
+}
 
-  return {
-    list: $('[data-testid=list]') as List,
-    input: $('[data-testid=input]') as HTMLInputElement & { validate: () => boolean },
-    button: $('[data-testid=button') as HTMLButtonElement,
+function testDisabled(disabled: boolean) {
+  return async (element: TestOriginsList) => {
+    await element.updateComplete;
+    const refs = getRefs<Refs>(element);
+
+    expect(refs.list).to.have.property('disabled', disabled);
+    expect(refs.input).to.have.property('disabled', disabled);
   };
 }
 
-function testEnabled(element: OriginsList) {
-  const refs = getRefs(element);
+function testInvalid(invalid: boolean) {
+  return async (element: TestOriginsList) => {
+    await element.updateComplete;
+    const refs = getRefs<Refs>(element);
 
-  expect(refs.list.disabled).to.be.false;
-  expect(refs.input.disabled).to.be.false;
-  expect(refs.button.disabled).to.be.false;
+    expect(refs.input).to.have.property('invalid', invalid);
+    expect(refs.button).to.have.property('disabled', element.disabled || invalid);
+  };
 }
 
-function testDisabled(element: OriginsList) {
-  const refs = getRefs(element);
+async function testValue(element: TestOriginsList) {
+  await element.updateComplete;
+  const refs = getRefs<Refs>(element);
 
-  expect(refs.list.disabled).to.be.true;
-  expect(refs.input.disabled).to.be.true;
-  expect(refs.button.disabled).to.be.true;
-}
-
-function testEmpty(element: OriginsList) {
-  expect(getRefs(element).list.value).to.be.empty;
-}
-
-function testInvalid(element: OriginsList) {
-  if (element.disabled) return;
-
-  const { input, button } = getRefs(element);
-  const value = input.value;
-  const triggers = [
-    () => input.dispatchEvent(new KeyboardEvent('keypress', { key: 'Enter' })),
-    () => button.click(),
-  ];
-
-  for (const trigger of triggers) {
-    trigger();
-    expect(element.value).not.to.include(value);
-    input.value = value;
-  }
-}
-
-async function testItems(element: OriginsList) {
-  const { list, input, button } = getRefs(element);
-
-  expect(list.value).to.deep.equal(element.value);
-  expect(list.value).to.deep.equal(samples.value);
-
-  if (!element.disabled) {
-    const newValue = samples.value.slice(0, 2);
-    let whenChanged = new Promise(res => element.addEventListener('change', res, { once: true }));
-
-    list.value = newValue;
-    list.dispatchEvent(new ListChangeEvent(newValue));
-
-    let event = (await whenChanged) as OriginsListChangeEvent;
-
-    expect(list.value).to.deep.equal(newValue);
-    expect(event.detail).to.deep.equal(newValue);
-
-    const triggers = [
-      () => input.dispatchEvent(new KeyboardEvent('keypress', { key: 'Enter' })),
-      () => button.click(),
-    ];
-
-    for (const trigger of triggers) {
-      const newItem = 'https://foxy.dev';
-      input.value = newItem;
-      input.dispatchEvent(new Event('change'));
-
-      whenChanged = new Promise(res => element.addEventListener('change', res, { once: true }));
-      trigger();
-      event = (await whenChanged) as OriginsListChangeEvent;
-
-      expect(event.detail[event.detail.length - 1]).to.equal(newItem);
-      expect(list.value).to.deep.equal(element.value);
-      expect(list.value).to.deep.equal(event.detail);
-      element.value = samples.value;
-    }
-  }
+  expect(refs.list).to.have.deep.property('value', element.value);
 }
 
 const machine = createMachine({
-  type: 'parallel',
+  id: 'root',
+  initial: 'enabled',
+  meta: { test: testValue },
   states: {
-    interactivity: {
-      initial: 'enabled',
-      meta: { test: () => true },
+    enabled: {
+      on: { ENTER_VALID: '.valid', ENTER_INVALID: '.invalid', CLEAR: '.clean.afterClear' },
+      meta: { test: testDisabled(false) },
+      initial: 'clean',
       states: {
-        enabled: { on: { DISABLE: 'disabled' }, meta: { test: testEnabled } },
-        disabled: { on: { ENABLE: 'enabled' }, meta: { test: testDisabled } },
+        clean: {
+          on: { DISABLE: '#root.disabled.clean' },
+          meta: { test: testInvalid(false) },
+          states: { any: {}, afterClear: {}, afterSubmit: {} },
+          initial: 'any',
+        },
+        valid: {
+          on: { DISABLE: '#root.disabled.valid', SUBMIT: 'clean.afterSubmit' },
+          meta: { test: testInvalid(false) },
+        },
+        invalid: {
+          on: { DISABLE: '#root.disabled.invalid' },
+          meta: { test: testInvalid(true) },
+        },
       },
     },
-    content: {
-      initial: 'empty',
-      meta: { test: () => true },
+    disabled: {
+      meta: { test: testDisabled(true) },
+      initial: 'clean',
       states: {
-        empty: { meta: { test: testEmpty } },
-        invalid: { meta: { test: testInvalid } },
-        withItems: { meta: { test: testItems } },
-      },
-      on: {
-        TEST_ITEMS: '.withItems',
-        INVALIDATE: '.invalid',
+        clean: { on: { ENABLE: '#root.enabled.clean' }, meta: { test: testInvalid(false) } },
+        valid: { on: { ENABLE: '#root.enabled.valid' }, meta: { test: testInvalid(false) } },
+        invalid: { on: { ENABLE: '#root.enabled.invalid' }, meta: { test: testInvalid(true) } },
       },
     },
   },
 });
 
-const model = createModel<OriginsList>(machine).withEvents({
-  ENABLE: { exec: element => void (element.disabled = false) },
-  DISABLE: { exec: element => void (element.disabled = true) },
-  TEST_ITEMS: { exec: element => void (element.value = samples.value) },
-  INVALIDATE: {
-    exec: (element, event) => {
+const model = createModel<TestOriginsList>(machine).withEvents({
+  CLEAR: {
+    exec: async function execClear(element) {
+      await element.updateComplete;
+      const refs = getRefs<Refs>(element);
+
+      refs.input.value = '';
+      refs.input.dispatchEvent(new InputEvent('input'));
+      await element.updateComplete;
+
+      refs.list.value = [];
+      refs.list.dispatchEvent(new ListChangeEvent([]));
+      await element.updateComplete;
+    },
+  },
+  SUBMIT: {
+    cases: [{ trigger: 'keyboard' }, { trigger: 'mouse' }],
+    exec: async function execSubmit(element, event) {
+      await element.updateComplete;
+
+      const refs = getRefs<Refs>(element);
+      const trigger = (event as EventObject & { trigger: string }).trigger;
+
+      if (trigger === 'keyboard') {
+        refs.input.dispatchEvent(new KeyboardEvent('keypress', { key: 'Enter' }));
+      } else {
+        refs.button.click();
+      }
+    },
+  },
+  ENABLE: {
+    exec: async function execEnable(element) {
+      element.disabled = false;
+      expect(element).to.have.property('disabled', false);
+    },
+  },
+  DISABLE: {
+    exec: async function execDisable(element) {
+      element.disabled = true;
+      expect(element).to.have.property('disabled', true);
+    },
+  },
+  ENTER_VALID: {
+    cases: [{ value: 'http://localhost:8080' }, { value: 'https://foxy.io' }],
+    exec: async function execEnterValid(element, event) {
+      await element.updateComplete;
+
       const value = ((event as unknown) as { value: string }).value;
-      const input = getRefs(element).input;
+      const input = getRefs<Refs>(element).input;
 
       input.value = value;
-      input.validate();
+      input.dispatchEvent(new InputEvent('input'));
+      input.dispatchEvent(new Event('change'));
     },
-    cases: [{ value: '     ' }, { value: 'httрs://cyrillic.culprit.local' }],
+  },
+  ENTER_INVALID: {
+    cases: [{ value: 'not a url' }, { value: 'http://insecure.foxy.io' }],
+    exec: async function execEnterInvalid(element, event) {
+      await element.updateComplete;
+
+      const value = ((event as unknown) as { value: string }).value;
+      const input = getRefs<Refs>(element).input;
+
+      input.value = value;
+      input.dispatchEvent(new InputEvent('input'));
+      input.dispatchEvent(new Event('change'));
+    },
   },
 });
 
@@ -146,14 +161,12 @@ describe('CustomerPortalSettings >>> OriginsList', () => {
   model.getSimplePathPlans().forEach(plan => {
     describe(plan.description, () => {
       plan.paths.forEach(path => {
-        it(path.description, async () =>
-          path.test(await fixture('<x-origins-list></x-origins-list>'))
-        );
+        it(path.description, async () => {
+          const element = await fixture<TestOriginsList>('<x-origins-list></x-origins-list>');
+          await element.whenReady;
+          return path.test(element);
+        });
       });
     });
-  });
-
-  describe('has full coverage', () => {
-    it('yes', () => model.testCoverage());
   });
 });
