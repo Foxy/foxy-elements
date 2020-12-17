27286d0657dfccc287f69744305d2184be48cdee
Daniil Bratukhin <dantothefuture@gmail.com>
2020-12-18 02:11:09 +0300

    wip: add loading and error states to default payment method

--- src/elements/public/FoxyDefaultPaymentMethodElement/FoxyDefaultPaymentMethodElement.ts
+++ src/elements/public/FoxyDefaultPaymentMethodElement/FoxyDefaultPaymentMethodElement.ts
@@ -4,9 +4,9 @@ import '@polymer/iron-icons';
 import * as FoxySDK from '@foxy.io/sdk';
 
 import { CSSResultArray, css } from 'lit-element';
+import { HypermediaResource, I18N, Skeleton } from '../../private';
 import { TemplateResult, html } from 'lit-html';
 
-import { HypermediaResource } from '../../private';
 import { ScopedElementsMap } from '@open-wc/scoped-elements';
 import { backgrounds } from './backgrounds';
 import { cdn } from '../../../env';
@@ -18,7 +18,9 @@ export class FoxyDefaultPaymentMethodElement extends HypermediaResource<Resource
 
   static get scopedElements(): ScopedElementsMap {
     return {
+      'x-skeleton': Skeleton,
       'iron-icon': customElements.get('iron-icon'),
+      'x-i18n': I18N,
     };
   }
 
@@ -51,18 +53,42 @@ export class FoxyDefaultPaymentMethodElement extends HypermediaResource<Resource
   }
 
   render(): TemplateResult {
-    if (!this.resource) return html`Loading...`;
+    const isLoading = this._is('loading');
+    const isError = this._is('error');
 
-    const type = this.resource.cc_type.toLowerCase();
+    if (isLoading || isError) {
+      return html`
+        <div class="ratio-card bg-base" aria-live="polite" aria-busy=${isLoading}>
+          <x-skeleton class="h-full" size="box" variant=${isError ? 'error' : 'busy'}></x-skeleton>
+
+          ${isError
+            ? html`
+                <div class="text-error flex flex-col justify-center absolute inset-0">
+                  <iron-icon icon="icons:error-outline" class="mb-xs mx-auto"></iron-icon>
+                  <x-i18n
+                    .ns=${this.ns}
+                    .lang=${this.lang}
+                    key="failed_to_load"
+                    class="text-s mx-auto"
+                  >
+                  </x-i18n>
+                </div>
+              `
+            : ''}
+        </div>
+      `;
+    }
+
+    const type = this.resource!.cc_type.toLowerCase();
     const logo = new URL(`./logos/${type}.svg`, cdn).toString();
-    const last4Digits = this.resource.cc_number_masked.substring(
-      this.resource.cc_number_masked.length - 4
+    const last4Digits = this.resource!.cc_number_masked.substring(
+      this.resource!.cc_number_masked.length - 4
     );
 
     return html`
       <div class="ratio-card">
         <div
-          class="flex flex-col justify-between text-base text-m leading-m font-lumo p-m bg-${type}"
+          class="flex flex-col justify-between text-base text-m leading-m font-lumo p-m bg-unknown bg-${type}"
         >
           <div class="flex items-start justify-between">
             <button
@@ -78,7 +104,7 @@ export class FoxyDefaultPaymentMethodElement extends HypermediaResource<Resource
           <div class="font-tnum leading-none flex justify-between">
             <div>
               <span class="sr-only">${this._t('expires').toString()}&nbsp;</span>
-              <span>${this.resource.cc_exp_month} / ${this.resource.cc_exp_year}</span>
+              <span>${this.resource!.cc_exp_month} / ${this.resource!.cc_exp_year}</span>
             </div>
 
             <div>
