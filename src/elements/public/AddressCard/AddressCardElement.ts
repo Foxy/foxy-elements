b4deaad069420a8dbd7aee4280bdec799f2d87ea
Daniil Bratukhin <dantothefuture@gmail.com>
2021-02-20 00:53:16 +0300

    refactor: remove deprecated members from address card

--- src/elements/public/AddressCard/AddressCardElement.ts
+++ src/elements/public/AddressCard/AddressCardElement.ts
@@ -33,16 +33,11 @@ export class AddressCardElement extends ScopedElementsMixin(NucleonElement)<Data
   private static __ns = 'address-card';
 
   render(): TemplateResult {
-    const { lang, href, state } = this;
-
-    const isFail = state.matches('fail');
-    const isIdle = state.matches('idle');
-
     const ns = AddressCardElement.__ns;
-    const variant = isFail ? 'error' : 'busy';
-    const icon = this.state.context.data?.is_default_billing
+    const variant = this.in('fail') ? 'error' : 'busy';
+    const icon = this.form.is_default_billing
       ? 'icons:payment'
-      : this.state.context.data?.is_default_shipping
+      : this.form.is_default_shipping
       ? 'maps:local-shipping'
       : '';
 
@@ -51,50 +46,53 @@ export class AddressCardElement extends ScopedElementsMixin(NucleonElement)<Data
         header="edit"
         parent=${this.parent}
         form="foxy-address-form"
-        lang=${lang}
-        href=${href}
+        lang=${this.lang}
+        href=${this.href}
         ns=${ns}
         id="form-dialog"
+        data-testid="dialog"
       >
       </foxy-form-dialog>
 
       <button
         class="text-left w-full flex items-start leading-m font-lumo space-x-m text-body focus:outline-none"
         aria-live="polite"
-        aria-busy=${state.matches('busy')}
+        aria-busy=${this.in('busy')}
+        data-testid="wrapper"
         @click=${this.__handleClick}
       >
         <div class="relative flex-1 leading-m">
           ${[1, 2, 3].map(lineIndex => {
-            const lineClass = classMap({ 'block text-m': true, 'opacity-0': isFail });
-            if (!isIdle) return html`<x-skeleton class=${lineClass}>&nbsp;</x-skeleton>`;
+            const lineClass = classMap({ 'block text-m': true, 'opacity-0': this.in('fail') });
+            if (!this.in('idle')) return html`<x-skeleton class=${lineClass}>&nbsp;</x-skeleton>`;
 
             return html`
               <foxy-i18n
                 ns=${ns}
                 key=${`line_${lineIndex}`}
-                lang=${lang}
+                lang=${this.lang}
                 class=${lineClass}
-                .opts=${state.context.data!}
+                data-testid="line${lineIndex}"
+                .opts=${this.form}
               >
               </foxy-i18n>
             `;
           })}
-          ${isFail
+          ${this.in('fail')
             ? html`
                 <div class="my-xs absolute text-error bg-error-10 rounded inset-0 flex">
                   <div class="flex m-auto items-center justify-center space-x-s">
                     <iron-icon icon="icons:error-outline"></iron-icon>
-                    <foxy-i18n ns=${ns} lang=${lang} key="error" class="text-s"></foxy-i18n>
+                    <foxy-i18n ns=${ns} lang=${this.lang} key="error" class="text-s"></foxy-i18n>
                   </div>
                 </div>
               `
             : ''}
         </div>
 
-        ${isIdle
+        ${this.in('idle')
           ? icon
-            ? html`<iron-icon icon=${icon}></iron-icon>`
+            ? html`<iron-icon icon=${icon} data-testid="icon"></iron-icon>`
             : ''
           : html`<x-skeleton class="w-s min-w-0" variant=${variant}></x-skeleton>`}
       </button>
