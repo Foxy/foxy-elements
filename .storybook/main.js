3f4b6089384b58186c3aa11c409bba4e7f306a72
Daniil Bratukhin <dantothefuture@gmail.com>
2021-03-02 14:17:04 +0300

    build: add the rest of sdk aliases to storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,8 @@ module.exports = {
     });
 
     config.resolve.alias['@foxy.io/sdk/core'] = '@foxy.io/sdk/dist/esm/core';
+    config.resolve.alias['@foxy.io/sdk/customer'] = '@foxy.io/sdk/dist/esm/customer';
+    config.resolve.alias['@foxy.io/sdk/integration'] = '@foxy.io/sdk/dist/esm/integration';
 
     return config;
   },
