e1b815630a6eb8a2ead62ec1cb40e3920da5ad3d
Daniil Bratukhin <dantothefuture@gmail.com>
2021-03-18 12:47:28 +0300

    feat: add foxy-spinner element

---
+++ src/elements/public/Spinner/index.ts
@@ -0,0 +1,10 @@
+import '@polymer/iron-icon';
+import '@polymer/iron-icons';
+import '@polymer/paper-spinner/paper-spinner-lite.js';
+import '../I18n/index';
+
+import { Spinner } from './Spinner';
+
+customElements.define('foxy-spinner', Spinner);
+
+export { Spinner };
