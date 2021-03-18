4ba0293d1e3f492ff1717151c8049b32686c966f
Daniil Bratukhin <dantothefuture@gmail.com>
2021-03-18 12:43:00 +0300

    build: add event-based mock api server

---
+++ src/server/admin/getPagination.ts
@@ -0,0 +1,9 @@
+export function getPagination(url: string) {
+  const limitInQuery = parseInt(new URL(url).searchParams.get('limit') ?? '');
+  const limit = isNaN(limitInQuery) || limitInQuery > 300 || limitInQuery < 0 ? 20 : limitInQuery;
+
+  const offsetInQuery = parseInt(new URL(url).searchParams.get('offset') ?? '');
+  const offset = isNaN(offsetInQuery) || offsetInQuery < 0 ? 0 : offsetInQuery;
+
+  return { limit, offset };
+}
