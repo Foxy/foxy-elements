6d016c50307848d573bac43557a2b1260e355f5d
Daniil Bratukhin <dantothefuture@gmail.com>
2021-03-18 12:58:38 +0300

    feat: add foxy-payment-method-card element

---
+++ src/elements/public/PaymentMethodCard/backgrounds.ts
@@ -0,0 +1,27 @@
+import { unsafeCSS } from 'lit-element';
+
+const pattern = unsafeCSS(`
+  repeating-radial-gradient(
+    circle at -100% 500%,
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 0.05) 4px
+  )
+`);
+
+const colors = {
+  unknown: '#797c85',
+  amex: '#2557d6',
+  diners: '#0079be',
+  discover: '#4d4d4d',
+  jcb: '#0e4c96',
+  maestro: '#181818',
+  mastercard: '#16366f',
+  unionpay: '#0dadb5',
+  visa: '#0e4595',
+};
+
+export const backgrounds = unsafeCSS(
+  Object.entries(colors)
+    .map(([card, color]) => `.bg-${card} { background: ${color} ${pattern} }`)
+    .join(' ')
+);
