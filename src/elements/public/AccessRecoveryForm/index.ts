4289f6b34f8f7cdd1706f51ab7e369ab25d6600c
Daniil Bratukhin <dantothefuture@gmail.com>
2021-04-06 17:03:17 +0300

    fix: add missing icon imports

--- src/elements/public/AccessRecoveryForm/index.ts
+++ src/elements/public/AccessRecoveryForm/index.ts
@@ -1,6 +1,6 @@
 import '@vaadin/vaadin-text-field/vaadin-email-field';
 import '@vaadin/vaadin-button';
-import '@polymer/iron-icons';
+import '@vaadin/vaadin-lumo-styles/icons';
 import '@polymer/iron-icon';
 import '../Spinner';
 import '../I18n';
--- src/elements/public/SignInForm/index.ts
+++ src/elements/public/SignInForm/index.ts
@@ -1,7 +1,7 @@
 import '@vaadin/vaadin-text-field/vaadin-email-field';
 import '@vaadin/vaadin-text-field/vaadin-password-field';
 import '@vaadin/vaadin-button';
-import '@polymer/iron-icons';
+import '@vaadin/vaadin-lumo-styles/icons';
 import '@polymer/iron-icon';
 import '../Spinner';
 import '../I18n';
