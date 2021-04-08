0ada8e43af100bdec07f49076fbdd1e377c5f38d
Daniil Bratukhin <dantothefuture@gmail.com>
2021-04-08 16:07:22 +0300

    fix: use simpler syntax for env vars so that the rollup replace plugin can recognize it in cdn builds

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,9 @@
-export const cdn = globalThis.process?.env?.FOXY_CDN ?? '';
+let cdn = '';
+
+try {
+  if (process.env.FOXY_CDN) cdn = process.env.FOXY_CDN;
+} catch {
+  // env vars are unavailable, use the default
+}
+
+export { cdn };
