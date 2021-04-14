93c770ec87a6607afa065ec804887b46607e9ec5
Daniil Bratukhin <dantothefuture@gmail.com>
2021-04-15 01:48:52 +0300

    fix: add missing imports

--- src/elements/public/Customer/index.ts
+++ src/elements/public/Customer/index.ts
@@ -4,13 +4,16 @@ import '@polymer/iron-icons/editor-icons';
 import '@vaadin/vaadin-button';
 import '../PaymentMethodCard/index';
 import '../SubscriptionsTable/index';
+import '../SubscriptionForm/index';
 import '../FormDialog/index';
 import '../TransactionsTable/index';
 import '../CollectionPages/index';
 import '../CollectionPage/index';
 import '../AttributeCard/index';
+import '../AttributeForm/index';
 import '../CustomerForm/index';
 import '../AddressCard/index';
+import '../AddressForm/index';
 import '../Spinner/index';
 import '../I18n/index';
 
