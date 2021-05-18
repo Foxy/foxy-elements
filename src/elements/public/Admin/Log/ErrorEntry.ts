42983e775efee7649cdc4c40b153c02cb68593dc
ndvo <nelson@ocastudios.com>
2021-05-17 23:34:08 -0300

    feat(adminlog): display get and post values

--- src/elements/public/Admin/Log/ErrorEntry.ts
+++ src/elements/public/Admin/Log/ErrorEntry.ts
@@ -1,8 +1,8 @@
 import type * as FoxySDK from '@foxy.io/sdk';
 import { ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements';
-import { NucleonElement } from '../../NucleonElement/NucleonElement';
+import { NucleonElement } from '../../NucleonElement';
 import { html } from 'lit-html';
-import { CSSResult, CSSResultArray, TemplateResult } from 'lit-element';
+import { CSSResult, CSSResultArray, LitElement, PropertyDeclarations, TemplateResult, css } from 'lit-element';
 import { Themeable } from '../../../../mixins/themeable';
 
 type ErrorEntries = FoxySDK.Backend.Rels.ErrorEntries;
@@ -36,7 +36,8 @@ export class ErrorEntry extends ScopedElementsMixin(NucleonElement)<Data> {
       'foxy-i18n': customElements.get('foxy-i18n'),
       'vaadin-button': customElements.get('vaadin-button'),
       'foxy-customer-info': CustomerInfo,
-      'foxy-transaction-info': TransactionInfo
+      'foxy-transaction-info': TransactionInfo,
+      'foxy-queryparams-viewer': URLSearchParamsViewer,
     }
   }
 
@@ -46,11 +47,11 @@ export class ErrorEntry extends ScopedElementsMixin(NucleonElement)<Data> {
       return html`
         <article class='m-s text-body'>
           <header class='border-l-2 h-full p-s relative ${this.open ? 'border-error': 'border-primary'}'>
-            <foxy-i18n key='date' options='{"value": "${i.date_created}"}' class='${this.open ? 'text-error': 'text-primary'}'></foxy-i18n>
-            <div>${i.error_message}</div>
+            <foxy-i18n key='date' options='{"value": "${i.date_created}"}' class='text-s ${this.open ? 'text-error': 'text-primary'}'></foxy-i18n>
+            <foxy-i18n key='time' options='{"value": "${i.date_created}"}' class='text-s ${this.open ? 'text-error': 'text-primary'}'></foxy-i18n>
             <vaadin-button @click=${this.__toggleOpen}
                            theme="icon"
-                           class='absolute right-s top-s rounded-full'
+                           class='text-s absolute right-s top-s rounded-full bg-transparent top-0 m-0 p-0'
                            aria-label='toggle'
             >
               ${this.open
@@ -58,24 +59,27 @@ export class ErrorEntry extends ScopedElementsMixin(NucleonElement)<Data> {
                 : html`<iron-icon icon='icons:expand-more'></iron-icon>`
               }
             </vaadin-button>
+            <div>${i.error_message}</div>
+
           </header>
           ${this.open
             ? html`
               <main>
                 <section class='my-s'>
-                  <h1 class='text-disabled text-m m-0 space-0'>Request</h1>
+                  <h1 class='text-tertiary text-m m-0 space-0'>Request</h1>
                   <main class='rounded-l border border-contrast-10 p-s'>
                     <p>${i.url}</p>
                     ${i.referrer
-                      ? html`<span>Navigated from</span> <span>${i.referrer}</span>`
+                      ? html`<span class='text-secondary'>Navigated from</span> <span>${i.referrer}</span>`
                       : html``
                     }
                     ${i.get_values
-                      ? html`<div>Here comes the get values ${i.get_values}</div>`
+                      ? html`
+                        <foxy-queryparams-viewer data='${i.get_values}' method='GET'></foxy-queryparams-viewer>`
                       : html``
                     }
                     ${i.post_values
-                      ? html`<div>Here comes the post values ${i.post_values}</div>`
+                      ? html`<foxy-queryparams-viewer data='${i.post_values}' method='POST'></foxy-queryparams-viewer>`
                       : html``
                     }
                   </main>
@@ -125,4 +129,87 @@ class TransactionInfo extends ScopedElementsMixin(NucleonElement)<FoxySDK.Core.R
       html`<a href='#'>${this.data.id} - ${this.data.currency_code}</a>`:
       html``;
   }
+}
+
+
+class URLSearchParamsViewer extends ScopedElementsMixin(LitElement) {
+
+  static get styles(): CSSResult | CSSResultArray {
+    return [
+      Themeable.styles,
+      css`
+        dt, dd {
+          display: inline-block;
+          overflow: hidden;
+          vertical-align: top;
+          overflow-wrap: anywhere;
+        }
+        dt:before {
+          display: block;
+          content: ' ';
+        }
+        dt {
+          min-width: 150px;
+          width: calc( 25% - 0.5em);
+          padding-right: 0.5em;
+        }
+        dd {
+          width: 75%;
+        }
+      `
+    ];
+  }
+
+  static get properties(): PropertyDeclarations {
+    return {
+      ...super.properties,
+      method: {
+        type: String
+      },
+      data: {
+        converter: {
+          fromAttribute(value: string): URLSearchParams {
+            return new URLSearchParams(decodeHtml(value));
+          },
+          toAttribute(value: URLSearchParams): string {
+            return value.toString();
+          }
+        },
+        type: Object
+      }
+    }
+  }
+
+  data: URLSearchParams;
+
+  method: 'GET'|'POST';
+
+  constructor() {
+    super();
+    this.data = new URLSearchParams();
+    this.method = 'GET';
+  }
+
+  render(): TemplateResult {
+    return html`
+      <div class='border-t-2 border-solid border-contrast-10 my-s relative overflow-hidden'>
+        <div class='text-tertiary text-l absolute right-s top-0'>${this.method}</div>
+        <dl class='mb-s'>
+          ${
+            Array.from(this.data.entries()).map(
+              (e) => {
+                return html`<dt class='text-secondary mt-s'>${e[0]}</dt><dd class='mt-s'>${e[1]}</dd>`;
+              })}
+        </dl>
+      </div>
+
+    `;
+  }
+}
+
+
+function decodeHtml(html) {
+    const areaElement = document.createElement("textarea");
+    areaElement.innerHTML = html;
+    return areaElement.value;
 }
