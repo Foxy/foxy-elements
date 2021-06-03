c10184e6082ce1b13456959787d396c84042db52
ndvo <nelson@ocastudios.com>
2021-06-02 22:24:35 -0300

    feat(demodb): add user table

--- src/server/admin/DemoDatabase.ts
+++ src/server/admin/DemoDatabase.ts
@@ -14,40 +14,43 @@ export class DemoDatabase extends Dexie {
     });
   }
 
-  customerAttributes: Dexie.Table<any, number>;
+  carts: Dexie.Table<any, number>;
 
   customerAddresses: Dexie.Table<any, number>;
 
+  customerAttributes: Dexie.Table<any, number>;
+
+  customers: Dexie.Table<any, number>;
+
   errorEntries: Dexie.Table<any, number>;
 
+  items: Dexie.Table<any, number>;
+
   paymentMethods: Dexie.Table<any, number>;
+
+  stores: Dexie.Table<any, number>;
 
   subscriptions: Dexie.Table<any, number>;
 
   transactions: Dexie.Table<any, number>;
 
-  customers: Dexie.Table<any, number>;
-
-  stores: Dexie.Table<any, number>;
-
-  items: Dexie.Table<any, number>;
-
-  carts: Dexie.Table<any, number>;
+  users: Dexie.Table<any, number>;
 
   constructor() {
     super('foxy_demo_db');
 
     this.version(1).stores({
-      customer_attributes: '++id,customer',
+      carts: '++id',
       customer_addresses: '++id,customer',
+      customer_attributes: '++id,customer',
+      customers: '++id,store',
       error_entries: '++id,store',
+      items: '++id,cart,transaction',
       payment_methods: '++id,customer',
+      stores: '++id',
       subscriptions: '++id,store,customer',
       transactions: '++id,store,customer,subscription',
-      customers: '++id,store',
-      stores: '++id',
-      items: '++id,cart,transaction',
-      carts: '++id',
+      users: '++id,store'
     });
 
     this.customerAttributes = this.table('customer_attributes');
@@ -60,5 +63,6 @@ export class DemoDatabase extends Dexie {
     this.stores = this.table('stores');
     this.items = this.table('items');
     this.carts = this.table('carts');
+    this.users = this.table('users');
   }
 }
