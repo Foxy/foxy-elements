cb249f8e9f2c60b064a06192b5555568d42f40e3
Daniil Bratukhin <dantothefuture@gmail.com>
2021-06-07 18:18:49 +0300

    refactor: explicitly define render property in internal sandbox element

--- src/elements/internal/InternalSandbox/InternalSandbox.ts
+++ src/elements/internal/InternalSandbox/InternalSandbox.ts
@@ -1,7 +1,9 @@
-import { LitElement, PropertyDeclarations } from 'lit-element';
+import { LitElement, PropertyDeclarations, TemplateResult, html } from 'lit-element';
 
 export class InternalSandbox extends LitElement {
   static get properties(): PropertyDeclarations {
     return { render: { attribute: false } };
   }
+
+  render: () => TemplateResult = () => html``;
 }
