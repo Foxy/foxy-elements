2a98b7bcd025197ed0dd6b71c2d7fd290621fd08
ndvo <nelson@ocastudios.com>
2021-06-13 10:07:55 -0300

    feat(taxform): add validation

--- src/elements/public/TaxForm/TaxForm.ts
+++ src/elements/public/TaxForm/TaxForm.ts
@@ -1,10 +1,25 @@
 import { Data } from './types';
 import { NucleonElement } from '../NucleonElement';
+import { NucleonV8N } from '../NucleonElement/types';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
 import { TemplateResult } from 'lit-element';
 import { html } from 'lit-html';
 
 export class TaxForm extends ScopedElementsMixin(NucleonElement)<Data> {
+  static get v8n(): NucleonV8N<Data> {
+    return [
+      ({ name: v }) => (v && v.length <= 30) || 'name_too_long',
+      ({ type: v }) =>
+        (v && ['global', 'country', 'region', 'local'].includes(v)) || 'type_unknown',
+      ({ country: v }) => !v || !!v.match(/[A-Z]{2}/) || 'country_unknown',
+      ({ region: v }) => !v || v.length <= 20 || 'region_too_long',
+      ({ city: v }) => !v || v.length <= 20 || 'city_too_long',
+      ({ city: c, type: t }) => (t == 'local' && c) || t != 'local' || 'city_too_long',
+      ({ service_provider: v }) => !v || v == 'avalara' || 'unknown_provider',
+      ({ rate: v }) => (v && v <= 100) || 'invalid_percentage',
+    ];
+  }
+
   render(): TemplateResult {
     return html``;
   }
