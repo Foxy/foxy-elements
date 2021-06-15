bef347ae852dbfb13f3854227c2739e48e905c8b
ndvo <nelson@ocastudios.com>
2021-06-15 17:59:58 -0300

    feat(taxform): add composeTax to dev server

---
+++ src/server/admin/composers/composeTax.ts
@@ -0,0 +1,16 @@
+import { endpoint } from '..';
+import halson from 'halson';
+import { HALJSONResource } from '../../../elements/public/NucleonElement/types';
+
+export function composeTax(doc: any): HALJSONResource {
+  const { id, store, ...publicData } = doc;
+  const result = halson({ ...publicData, id })
+    .addLink('self', `${endpoint}/taxes/${id}`)
+    .addLink('fx:store', `${endpoint}/stores/${store}`)
+    .addLink('fx:tax_item_categories', `${endpoint}/taxes/${id}/tax_item_categories`)
+    .addLink(
+      'fx:native_integrations',
+      `${endpoint}/native_integrations?provider=/${doc.service_provider}`
+    );
+  return result;
+}
