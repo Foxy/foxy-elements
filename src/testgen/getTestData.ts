22489f07e33a4e894958d8f99a39954a1e851877
Daniil Bratukhin <dantothefuture@gmail.com>
2021-07-02 17:07:21 +0300

    test: inject auth info to all test data requests to /s/customer

--- src/testgen/getTestData.ts
+++ src/testgen/getTestData.ts
@@ -3,5 +3,11 @@ import { router } from '../server/index';
 export async function getTestData<T>(path: string): Promise<T> {
   const url = new URL(path, 'https://demo.foxycart.com/');
   const request = new Request(url.toString());
+
+  if (path.includes('/s/customer')) {
+    const token = `0-${Date.now() + Math.pow(10, 10)}`;
+    request.headers.set('Authorization', `Bearer ${token}`);
+  }
+
   return router.handleRequest(request)!.handlerPromise.then(r => r.json());
 }
