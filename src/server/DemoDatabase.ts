50b823a6bfeb56c45fe19c538c37c7c29582d69e
ndvo <nelson@ocastudios.com>
2021-07-07 17:42:17 -0300

    chore(demodatabase.ts): add tax table

--- src/server/DemoDatabase.ts
+++ src/server/DemoDatabase.ts
@@ -14,29 +14,29 @@ class DemoDatabase extends Dexie {
     });
   }
 
-  customerPortalSettings: Dexie.Table<any, number>;
-
-  customerAttributes: Dexie.Table<any, number>;
+  carts: Dexie.Table<any, number>;
 
   customerAddresses: Dexie.Table<any, number>;
 
-  errorEntries: Dexie.Table<any, number>;
+  customerAttributes: Dexie.Table<any, number>;
 
-  paymentMethods: Dexie.Table<any, number>;
-
-  subscriptions: Dexie.Table<any, number>;
-
-  transactions: Dexie.Table<any, number>;
-
-  errorEntries: Dexie.Table<any, number>;
+  customerPortalSettings: Dexie.Table<any, number>;
 
   customers: Dexie.Table<any, number>;
 
-  stores: Dexie.Table<any, number>;
+  errorEntries: Dexie.Table<any, number>;
 
   items: Dexie.Table<any, number>;
 
-  carts: Dexie.Table<any, number>;
+  paymentMethods: Dexie.Table<any, number>;
+
+  stores: Dexie.Table<any, number>;
+
+  subscriptions: Dexie.Table<any, number>;
+
+  taxes: Dexie.Table<any, number>;
+
+  transactions: Dexie.Table<any, number>;
 
   users: Dexie.Table<any, number>;
 
@@ -44,32 +44,34 @@ class DemoDatabase extends Dexie {
     super('foxy_demo_db');
 
     this.version(4).stores({
-      customer_portal_settings: 'store',
-      customer_attributes: '++id,customer',
-      customer_addresses: '++id,customer',
-      error_entries: '++id',
-      payment_methods: '++id,customer',
-      subscriptions: '++id,store,customer',
-      transactions: '++id,store,customer,subscription',
-      customers: '++id,store',
-      stores: '++id',
-      items: '++id,cart,transaction',
       carts: '++id',
+      customer_addresses: '++id,customer',
+      customer_attributes: '++id,customer',
+      customer_portal_settings: 'store',
+      customers: '++id,store',
+      error_entries: '++id',
+      items: '++id,cart,transaction',
+      payment_methods: '++id,customer',
+      stores: '++id',
+      subscriptions: '++id,store,customer',
+      taxes: '++id,store',
+      transactions: '++id,store,customer,subscription',
       users: '++id,store',
     });
 
-    this.customerPortalSettings = this.table('customer_portal_settings');
-    this.customerAttributes = this.table('customer_attributes');
+    this.carts = this.table('carts');
     this.customerAddresses = this.table('customer_addresses');
+    this.customerAttributes = this.table('customer_attributes');
+    this.customerPortalSettings = this.table('customer_portal_settings');
+    this.customers = this.table('customers');
     this.errorEntries = this.table('error_entries');
+    this.errorEntries = this.table('error_entries');
+    this.items = this.table('items');
     this.paymentMethods = this.table('payment_methods');
+    this.stores = this.table('stores');
     this.subscriptions = this.table('subscriptions');
     this.transactions = this.table('transactions');
-    this.errorEntries = this.table('error_entries');
-    this.customers = this.table('customers');
-    this.stores = this.table('stores');
-    this.items = this.table('items');
-    this.carts = this.table('carts');
+    this.taxes = this.table('taxes');
     this.users = this.table('users');
   }
 }
