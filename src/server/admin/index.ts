45391a75ae95ed5e9bba7a6e905986b540427c78
ndvo <nelson@ocastudios.com>
2021-09-25 10:19:46 -0300

    style(server/admin/index): sort attributes

--- src/server/admin/index.ts
+++ src/server/admin/index.ts
@@ -1,5 +1,6 @@
+import { Collection, Table } from 'dexie';
 import { DemoDatabase, db, whenDbReady } from '../DemoDatabase';
-
+import { HALJSONResource } from '../../elements/public/NucleonElement/types';
 import { composeCollection } from './composers/composeCollection';
 import { composeCustomer } from './composers/composeCustomer';
 import { composeCustomerAddress } from './composers/composeCustomerAddress';
@@ -67,10 +68,10 @@ router.get('/s/admin/stores/:id/subscriptions', async ({ params, request }) => {
   );
 
   const items = subscriptions.map((subscription, index) => ({
-    transactionTemplate: embeddedTransactionTemplateBySubscription[index],
-    lastTransaction: embeddedLastTransactionBySubscription[index],
     items: embeddedItemsBySubscription[index],
+    lastTransaction: embeddedLastTransactionBySubscription[index],
     subscription,
+    transactionTemplate: embeddedTransactionTemplateBySubscription[index],
   }));
 
   const composeItem = ({ subscription, items, lastTransaction, transactionTemplate }: any) => {
@@ -78,7 +79,7 @@ router.get('/s/admin/stores/:id/subscriptions', async ({ params, request }) => {
   };
 
   const rel = 'fx:subscriptions';
-  const body = composeCollection({ composeItem, rel, url, count, items });
+  const body = composeCollection({ composeItem, count, items, rel, url });
 
   return new Response(JSON.stringify(body));
 });
@@ -124,8 +125,8 @@ router.get('/s/admin/stores/:id/transactions', async ({ params, request }) => {
   );
 
   const items = transactions.map((transaction, index) => ({
-    transaction,
     items: embeddedItemsByTransaction[index],
+    transaction,
   }));
 
   const composeItem = ({ transaction, items }: any) => {
@@ -133,7 +134,7 @@ router.get('/s/admin/stores/:id/transactions', async ({ params, request }) => {
   };
 
   const rel = 'fx:transactions';
-  const body = composeCollection({ composeItem, rel, url, count, items });
+  const body = composeCollection({ composeItem, count, items, rel, url });
 
   return new Response(JSON.stringify(body));
 });
@@ -189,7 +190,6 @@ router.get('/s/admin/subscriptions/:id', async ({ params, request }) => {
   const id = parseInt(params.id);
   const doc = await db.subscriptions.get(id);
   const zoom = new URL(request.url).searchParams.get('zoom') ?? '';
-
   const lastTransaction = zoom.includes('last_transaction')
     ? await db.transactions.where('subscription').equals(id).last()
     : undefined;
@@ -242,7 +242,7 @@ router.get('/s/admin/stores/:id/error_entries', async ({ params, request }) => {
     }
     const rel = 'fx:error_entries';
     const composeItem = composeErrorEntry;
-    const body = composeCollection({ composeItem, rel, url, count, items });
+    const body = composeCollection({ composeItem, count, items, rel, url });
     return new Response(JSON.stringify(body));
   } catch (e) {
     console.log('There was an error', e);
@@ -277,7 +277,7 @@ router.get('/s/admin/customers/:id/attributes', async ({ params, request }) => {
   ]);
   const rel = 'fx:attributes';
   const composeItem = composeCustomerAttribute;
-  const body = composeCollection({ composeItem, rel, url, count, items });
+  const body = composeCollection({ composeItem, count, items, rel, url });
 
   return new Response(JSON.stringify(body));
 });
@@ -296,12 +296,12 @@ router.post('/s/admin/customers/:id/attributes', async ({ params, request }) => 
 
   const requestBody = await request.json();
   const newID = await db.customerAttributes.add({
-    name: requestBody.name ?? '',
-    value: requestBody.value ?? '',
     customer: parseInt(params.id),
-    visibility: requestBody.visibility ?? 'private',
     date_created: new Date().toISOString(),
     date_modified: new Date().toISOString(),
+    name: requestBody.name ?? '',
+    value: requestBody.value ?? '',
+    visibility: requestBody.visibility ?? 'private',
   });
 
   const newDoc = await db.customerAttributes.get(newID);
@@ -346,7 +346,7 @@ router.get('/s/admin/customers/:id/addresses', async ({ params, request }) => {
 
   const rel = 'fx:customer_addresses';
   const composeItem = composeCustomerAddress;
-  const body = composeCollection({ composeItem, rel, url, count, items });
+  const body = composeCollection({ composeItem, count, items, rel, url });
 
   return new Response(JSON.stringify(body));
 });
@@ -357,24 +357,24 @@ router.post('/s/admin/customers/:id/addresses', async ({ params, request }) => {
   const customer = await db.customers.get(parseInt(params.id));
   const requestBody = await request.json();
   const newID = await db.customerAddresses.add({
-    store: customer.store,
-    customer: customer.id,
-    address_name: requestBody.address_name ?? '',
-    first_name: requestBody.first_name ?? '',
-    last_name: requestBody.last_name ?? '',
-    company: requestBody.company ?? '',
     address1: requestBody.address1,
     address2: requestBody.address2 ?? '',
+    address_name: requestBody.address_name ?? '',
     city: requestBody.city ?? '',
-    region: requestBody.region ?? '',
-    postal_code: requestBody.postal_code ?? '',
+    company: requestBody.company ?? '',
     country: requestBody.country ?? '',
-    phone: requestBody.phone ?? '',
-    is_default_billing: requestBody.is_default_billing ?? false,
-    is_default_shipping: requestBody.is_default_shipping ?? false,
-    ignore_address_restrictions: requestBody.ignore_address_restrictions ?? false,
+    customer: customer.id,
     date_created: new Date().toISOString(),
     date_modified: new Date().toISOString(),
+    first_name: requestBody.first_name ?? '',
+    ignore_address_restrictions: requestBody.ignore_address_restrictions ?? false,
+    is_default_billing: requestBody.is_default_billing ?? false,
+    is_default_shipping: requestBody.is_default_shipping ?? false,
+    last_name: requestBody.last_name ?? '',
+    phone: requestBody.phone ?? '',
+    postal_code: requestBody.postal_code ?? '',
+    region: requestBody.region ?? '',
+    store: customer.store,
   });
 
   const newDoc = await db.customerAddresses.get(newID);
@@ -427,7 +427,7 @@ router.get('/s/admin/stores/:id/customers', async ({ params, request }) => {
 
   const rel = 'fx:customers';
   const composeItem = composeCustomer;
-  const body = composeCollection({ composeItem, rel, url, count, items });
+  const body = composeCollection({ composeItem, count, items, rel, url });
 
   return new Response(JSON.stringify(body));
 });
@@ -492,7 +492,7 @@ router.get('/s/admin/stores/:id/users', async ({ params, request }) => {
     db.users.where('store').equals(store).offset(offset).limit(limit).toArray(),
   ]);
 
-  const body = composeCollection({ composeItem: composeUser, rel, url, count, items });
+  const body = composeCollection({ composeItem: composeUser, count, items, rel, url });
   return new Response(JSON.stringify(body));
 });
 
@@ -501,18 +501,18 @@ router.post('/s/admin/stores/:id/users', async ({ params, request }) => {
 
   const body = await request.json();
   const id = await db.users.add({
-    store: parseInt(params.id),
-    first_name: body.first_name ?? '',
-    last_name: body.last_name ?? '',
-    email: body.email ?? '',
-    phone: body.phone ?? '',
     affiliate_id: body.affiliate_id ?? 0,
-    is_programmer: body.is_programmer ?? false,
-    is_front_end_developer: body.is_front_end_developer ?? false,
-    is_designer: body.is_designer ?? false,
-    is_merchant: body.is_merchant ?? false,
     date_created: new Date().toISOString(),
     date_modified: new Date().toISOString(),
+    email: body.email ?? '',
+    first_name: body.first_name ?? '',
+    is_designer: body.is_designer ?? false,
+    is_front_end_developer: body.is_front_end_developer ?? false,
+    is_merchant: body.is_merchant ?? false,
+    is_programmer: body.is_programmer ?? false,
+    last_name: body.last_name ?? '',
+    phone: body.phone ?? '',
+    store: parseInt(params.id),
   });
 
   return router.handle(`/s/admin/users/${id}`, 'GET')!.handlerPromise;
