448466fef5fe94eecc860c817c01e1071ec76bb6
ndvo <nelson@ocastudios.com>
2021-09-25 11:06:09 -0300

    fix: add template fields to demodatabase

--- src/server/DemoDatabase.ts
+++ src/server/DemoDatabase.ts
@@ -14,27 +14,41 @@ class DemoDatabase extends Dexie {
     });
   }
 
-  customerPortalSettings: Dexie.Table<any, number>;
+  carts: Dexie.Table<any, number>;
 
-  customerAttributes: Dexie.Table<any, number>;
+  cartTemplates: Dexie.Table<any, number>;
+
+  cartIncludeTemplates: Dexie.Table<any, number>;
+
+  checkoutTemplates: Dexie.Table<any, number>;
 
   customerAddresses: Dexie.Table<any, number>;
 
-  paymentMethods: Dexie.Table<any, number>;
+  customerAttributes: Dexie.Table<any, number>;
 
-  subscriptions: Dexie.Table<any, number>;
-
-  transactions: Dexie.Table<any, number>;
-
-  errorEntries: Dexie.Table<any, number>;
+  customerPortalSettings: Dexie.Table<any, number>;
 
   customers: Dexie.Table<any, number>;
 
-  stores: Dexie.Table<any, number>;
+  emailTemplates: Dexie.Table<any, number>;
+
+  errorEntries: Dexie.Table<any, number>;
 
   items: Dexie.Table<any, number>;
 
-  carts: Dexie.Table<any, number>;
+  paymentMethods: Dexie.Table<any, number>;
+
+  receiptTemplates: Dexie.Table<any, number>;
+
+  stores: Dexie.Table<any, number>;
+
+  subscriptions: Dexie.Table<any, number>;
+
+  taxes: Dexie.Table<any, number>;
+
+  templateConfig: Dexie.Table<any, number>;
+
+  transactions: Dexie.Table<any, number>;
 
   users: Dexie.Table<any, number>;
 
@@ -42,31 +56,45 @@ class DemoDatabase extends Dexie {
     super('foxy_demo_db');
 
     this.version(5).stores({
-      customer_portal_settings: 'store',
-      customer_attributes: '++id,customer',
-      customer_addresses: '++id,customer',
-      payment_methods: '++id,customer',
-      subscriptions: '++id,store,customer',
-      transactions: '++id,store,customer,subscription',
-      error_entries: '++id',
-      customers: '++id,store,email',
-      stores: '++id',
-      items: '++id,cart,transaction',
+      cart_include_templates: '++id,store',
+      cart_templates: '++id,store',
       carts: '++id',
+      checkout_templates: '++id,store',
+      customer_addresses: '++id,customer',
+      customer_attributes: '++id,customer',
+      customer_portal_settings: 'store',
+      customers: '++id,store',
+      email_templates: '++id,store',
+      error_entries: '++id',
+      items: '++id,cart,transaction',
+      payment_methods: '++id,customer',
+      receipt_templates: '++id,store',
+      stores: '++id',
+      subscriptions: '++id,store,customer',
+      taxes: '++id,store',
+      template_config: '++id',
+      transactions: '++id,store,customer,subscription',
       users: '++id,store',
     });
 
-    this.customerPortalSettings = this.table('customer_portal_settings');
-    this.customerAttributes = this.table('customer_attributes');
-    this.customerAddresses = this.table('customer_addresses');
-    this.paymentMethods = this.table('payment_methods');
-    this.subscriptions = this.table('subscriptions');
-    this.transactions = this.table('transactions');
-    this.errorEntries = this.table('error_entries');
-    this.customers = this.table('customers');
-    this.stores = this.table('stores');
-    this.items = this.table('items');
+    this.cartIncludeTemplates = this.table('cart_include_templates');
+    this.cartTemplates = this.table('cart_templates');
     this.carts = this.table('carts');
+    this.checkoutTemplates = this.table('checkout_templates');
+    this.customerAddresses = this.table('customer_addresses');
+    this.customerAttributes = this.table('customer_attributes');
+    this.customerPortalSettings = this.table('customer_portal_settings');
+    this.customers = this.table('customers');
+    this.emailTemplates = this.table('email_templates');
+    this.errorEntries = this.table('error_entries');
+    this.items = this.table('items');
+    this.paymentMethods = this.table('payment_methods');
+    this.receiptTemplates = this.table('receipt_templates');
+    this.stores = this.table('stores');
+    this.subscriptions = this.table('subscriptions');
+    this.taxes = this.table('taxes');
+    this.templateConfig = this.table('template_config');
+    this.transactions = this.table('transactions');
     this.users = this.table('users');
   }
 }
