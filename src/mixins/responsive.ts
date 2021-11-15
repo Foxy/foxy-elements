044e715b9ee52a5038dc3c9d631a10c2e65195dc
Daniil Bratukhin <dantothefuture@gmail.com>
2021-11-15 22:34:44 +0300

    fix: run element resize handler at most 60 times a second

--- src/mixins/responsive.ts
+++ src/mixins/responsive.ts
@@ -2,28 +2,32 @@
 
 import { Constructor, LitElement } from 'lit-element';
 
+import debounce from 'lodash-es/debounce';
+
+const breakpoints = Object.entries({ sm: 640, md: 768, lg: 1024, xl: 1280 });
+
 export const ResponsiveMixin = <TBase extends Constructor<LitElement>>(
   BaseElement: TBase
 ): TBase => {
   return class ResponsiveElement extends BaseElement {
     private __removeBreakpoints?: () => void;
 
+    private __handleResize = debounce((entries: ResizeObserverEntry[]) => {
+      entries.forEach(({ contentRect, target }) => {
+        breakpoints.forEach(([name, minWidth]) => {
+          if (contentRect.width >= minWidth) {
+            if (!target.hasAttribute(name)) target.setAttribute(name, '');
+          } else {
+            if (target.hasAttribute(name)) target.removeAttribute(name);
+          }
+        });
+      });
+    }, 16);
+
     connectedCallback(): void {
       super.connectedCallback();
 
-      const breakpoints = Object.entries({ sm: 640, md: 768, lg: 1024, xl: 1280 });
-      const observer = new ResizeObserver(entries => {
-        entries.forEach(({ contentRect, target }) => {
-          breakpoints.forEach(([name, minWidth]) => {
-            if (contentRect.width >= minWidth) {
-              if (!target.hasAttribute(name)) target.setAttribute(name, '');
-            } else {
-              if (target.hasAttribute(name)) target.removeAttribute(name);
-            }
-          });
-        });
-      });
-
+      const observer = new ResizeObserver(this.__handleResize);
       observer.observe(this);
       this.__removeBreakpoints = () => observer.disconnect();
     }
@@ -31,6 +35,7 @@ export const ResponsiveMixin = <TBase extends Constructor<LitElement>>(
     disconnectedCallback(): void {
       super.disconnectedCallback();
       this.__removeBreakpoints?.();
+      this.__handleResize.cancel();
     }
   };
 };
