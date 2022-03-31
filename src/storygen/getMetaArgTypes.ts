3149be813bdbc9a2877cafcb9b7efdfe19b49b17
Daniil Bratukhin <dantothefuture@gmail.com>
2022-04-01 00:23:44 +0300

    docs: remove all language options except for `en` by default in storybook controls

--- src/storygen/getMetaArgTypes.ts
+++ src/storygen/getMetaArgTypes.ts
@@ -39,7 +39,10 @@ export function getMetaArgTypes(summary: Summary): Record<string, unknown> {
   }
 
   if (summary.translatable) {
-    argTypes.lang = { control: { type: 'inline-radio', options: ['en', 'es', 'de'] } };
+    argTypes.lang = {
+      control: { type: 'inline-radio', options: ['en'] },
+    };
+
     argTypes.ns = { control: false, table: { category: 'properties' } };
     argTypes.t = { control: false, table: { category: 'properties' } };
   }
