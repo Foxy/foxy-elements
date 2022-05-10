9f0dee5b076a32eea41c01ff10bf986db618110f
Daniil Bratukhin <dantothefuture@gmail.com>
2022-05-10 22:18:26 +0300

    fix(foxy-internal-number-control): fix base control class import

--- src/elements/internal/InternalNumberControl/index.ts
+++ src/elements/internal/InternalNumberControl/index.ts
@@ -1,5 +1,5 @@
 import '@vaadin/vaadin-text-field/vaadin-number-field';
-import '../InternalNumberControl/index';
+import '../InternalEditableControl/index';
 import { InternalNumberControl } from './InternalNumberControl';
 
 customElements.define('foxy-internal-number-control', InternalNumberControl);
