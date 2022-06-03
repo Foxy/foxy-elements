4c3399e5286c3004c88768d64460850c3102e9bd
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-03 21:05:01 +0400

    docs(foxy-internal-create-control): add jsdoc

--- src/elements/internal/InternalCreateControl/InternalCreateControl.ts
+++ src/elements/internal/InternalCreateControl/InternalCreateControl.ts
@@ -3,6 +3,13 @@ import type { TemplateResult, PropertyDeclarations } from 'lit-element';
 import { InternalControl } from '../InternalControl/InternalControl';
 import { html } from 'lit-element';
 
+/**
+ * Internal control displaying a "Create" button that triggers Nucleon form
+ * submission on click. The button is disabled if the form is invalid.
+ *
+ * @since 1.17.0
+ * @tag foxy-internal-create-control
+ */
 export class InternalCreateControl extends InternalControl {
   static get properties(): PropertyDeclarations {
     return {
@@ -11,20 +18,18 @@ export class InternalCreateControl extends InternalControl {
     };
   }
 
-  infer = 'create';
-
+  /** Same as the "theme" attribute of the `vaadin-button` element. */
   theme = 'primary success';
 
   renderControl(): TemplateResult {
     return html`
       <vaadin-button
-        data-testid="create"
         class="w-full"
         theme=${this.theme}
         ?disabled=${this.disabled || this.__isInvalid}
         @click=${() => this.nucleon?.submit()}
       >
-        <foxy-i18n ns=${this.ns} key="create" lang=${this.lang}></foxy-i18n>
+        <foxy-i18n infer="" key="create"></foxy-i18n>
       </vaadin-button>
     `;
   }
