b0b8a4192b49e581c15647f545415337bf466821
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-07 00:30:12 +0400

    fix(foxy-internal-delete-control): add missing imports

--- src/elements/internal/InternalDeleteControl/index.ts
+++ src/elements/internal/InternalDeleteControl/index.ts
@@ -1,6 +1,9 @@
-import '../InternalConfirmDialog/index';
-import '../../public/I18n/index';
 import '@vaadin/vaadin-button';
+
+import '../../public/I18n/index';
+import '../InternalConfirmDialog/index';
+import '../InternalControl/index';
+
 import { InternalDeleteControl } from './InternalDeleteControl';
 
 customElements.define('foxy-internal-delete-control', InternalDeleteControl);
