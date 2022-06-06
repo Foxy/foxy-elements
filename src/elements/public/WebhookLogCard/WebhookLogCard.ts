0fb57ca9f910d8350c925f112cde925949c1743f
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-07 00:47:03 +0400

    refactor(foxy-webhook-log-card): improve readability

--- src/elements/public/WebhookLogCard/WebhookLogCard.ts
+++ src/elements/public/WebhookLogCard/WebhookLogCard.ts
@@ -13,18 +13,18 @@ import { html } from 'lit-html';
  */
 export class WebhookLogCard extends TranslatableMixin(InternalCard, 'webhook-log-card')<Data> {
   renderBody(): TemplateResult {
+    const { date_created, response_code, response_body } = this.data ?? {};
+
     return html`
       <div class="leading-none space-y-xs">
         <p class="flex justify-between items-center font-semibold">
-          <foxy-i18n .options=${{ value: this.data?.date_created }} infer="" key="date">
-          </foxy-i18n>
-
+          <foxy-i18n .options=${{ value: date_created }} infer="" key="date"></foxy-i18n>
           <span class="inline-block text-xs bg-contrast-5 text-secondary rounded p-xs">
-            ${this.data?.response_code}&ZeroWidthSpace;
+            ${response_code}&ZeroWidthSpace;
           </span>
         </p>
 
-        <p class="text-tertiary">${this.data?.response_body}&ZeroWidthSpace;</p>
+        <p class="text-tertiary">${response_body}&ZeroWidthSpace;</p>
       </div>
     `;
   }
