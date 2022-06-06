6383af67971c746e09aedc9cc0ebbab1d2acc375
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-07 00:26:42 +0400

    fix(foxy-internal-create-control): add missing imports

--- src/elements/internal/InternalCreateControl/index.ts
+++ src/elements/internal/InternalCreateControl/index.ts
@@ -1,5 +1,8 @@
-import '../../public/I18n/index';
 import '@vaadin/vaadin-button';
+
+import '../../public/I18n/index';
+import '../InternalControl/index';
+
 import { InternalCreateControl } from './InternalCreateControl';
 
 customElements.define('foxy-internal-create-control', InternalCreateControl);
