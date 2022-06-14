4ce95ae420632796847aca25a26651f0f70b7a42
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-14 23:45:07 +0400

    fix(foxy-internal-control): add missing imports

--- src/elements/internal/InternalControl/index.ts
+++ src/elements/internal/InternalControl/index.ts
@@ -1,3 +1,6 @@
+import '../InternalSandbox/index';
+import '../../public/I18n/index';
+
 import { InternalControl } from './InternalControl';
 
 customElements.define('foxy-internal-control', InternalControl);
