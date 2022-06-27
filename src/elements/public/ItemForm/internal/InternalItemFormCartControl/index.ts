d043c486d0bdccf2fafb0c1b5fbc7ca328def1f4
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-27 18:17:33 +0400

    fix(foxy-internal-item-form-cart-control): add missing imports

--- src/elements/public/ItemForm/internal/InternalItemFormCartControl/index.ts
+++ src/elements/public/ItemForm/internal/InternalItemFormCartControl/index.ts
@@ -1,7 +1,9 @@
-import '../../../../internal/InternalDetails/index';
 import '../../../../internal/InternalIntegerControl/index';
 import '../../../../internal/InternalTextControl/index';
 import '../../../../internal/InternalDateControl/index';
+import '../../../../internal/InternalDetails/index';
+import '../../../../internal/InternalControl/index';
+
 import { InternalItemFormCartControl } from './InternalItemFormCartControl';
 
 customElements.define('foxy-internal-item-form-cart-control', InternalItemFormCartControl);
