fdeb09f6d23745d1adbd3aeb6bf9c357ec7bf474
Daniil Bratukhin <dantothefuture@gmail.com>
2022-06-27 18:21:53 +0400

    fix(foxy-internal-item-form-inventory-control): add missing imports

--- src/elements/public/ItemForm/internal/InternalItemFormInventoryControl/index.ts
+++ src/elements/public/ItemForm/internal/InternalItemFormInventoryControl/index.ts
@@ -1,6 +1,8 @@
 import '../../../../internal/InternalAsyncComboBoxControl/index';
-import '../../../../internal/InternalDetails/index';
 import '../../../../internal/InternalTextControl/index';
+import '../../../../internal/InternalDetails/index';
+import '../../../../internal/InternalControl/index';
+
 import { InternalItemFormInventoryControl as Control } from './InternalItemFormInventoryControl';
 
 customElements.define('foxy-internal-item-form-inventory-control', Control);
