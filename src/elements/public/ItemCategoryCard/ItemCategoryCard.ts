49ca2b6496c679ea10b46aea0ae26410297a4e22
Daniil Bratukhin <dantothefuture@gmail.com>
2022-08-31 23:06:47 +0300

    feat(foxy-item-category-card): update to single line design

--- src/elements/public/ItemCategoryCard/ItemCategoryCard.ts
+++ src/elements/public/ItemCategoryCard/ItemCategoryCard.ts
@@ -1,14 +1,20 @@
 import type { TemplateResult } from 'lit-html';
 import type { Data } from './types';
 
-import { TwoLineCard } from '../CustomFieldCard/TwoLineCard';
+import { InternalCard } from '../../internal/InternalCard/InternalCard';
 import { html } from 'lit-html';
 
-export class ItemCategoryCard extends TwoLineCard<Data> {
-  render(): TemplateResult {
-    return super.render({
-      title: data => html`${data.name}`,
-      subtitle: data => html`${data.code}`,
-    });
+export class ItemCategoryCard extends InternalCard<Data> {
+  renderBody(): TemplateResult {
+    const data = this.data;
+
+    return html`
+      <div class="flex justify-between gap-s">
+        <div class="font-semibold truncate flex-shrink-0">${data?.name}&ZeroWidthSpace;</div>
+        ${data?.name !== data?.code
+          ? html`<div class="truncate text-tertiary">${data?.code}</div>`
+          : ''}
+      </div>
+    `;
   }
 }
--- src/elements/public/ItemCategoryCard/index.ts
+++ src/elements/public/ItemCategoryCard/index.ts
@@ -1,3 +1,5 @@
+import '../../internal/InternalCard/index';
+
 import { ItemCategoryCard } from './ItemCategoryCard';
 
 customElements.define('foxy-item-category-card', ItemCategoryCard);
