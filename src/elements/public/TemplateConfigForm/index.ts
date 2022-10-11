13f5716df4ddb52c3fb6535909c8220272d0544e
Daniil Bratukhin <dantothefuture@gmail.com>
2022-10-11 01:42:52 -0500

    feat(foxy-template-config-form): separate config settings into 4 tabs

--- src/elements/public/TemplateConfigForm/index.ts
+++ src/elements/public/TemplateConfigForm/index.ts
@@ -2,6 +2,7 @@ import '@polymer/iron-icons';
 import '@polymer/iron-icon';
 import '@vaadin/vaadin-text-field/vaadin-text-field';
 import '@vaadin/vaadin-text-field/vaadin-text-area';
+import '@vaadin/vaadin-tabs';
 import '../../internal/InternalSandbox/index';
 import '../Spinner/index';
 import '../I18n/index';
