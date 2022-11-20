7da94b1835e4dc6444b3880807a4443b8177ddd5
Daniil Bratukhin <dantothefuture@gmail.com>
2022-11-20 17:42:01 -0600

    fix: measure element size immediately after connection to avoid a flash of mobile layout on devices with larger screens

--- src/mixins/responsive.ts
+++ src/mixins/responsive.ts
@@ -2,7 +2,7 @@
 
 import { Constructor, LitElement } from 'lit-element';
 
-import debounce from 'lodash-es/debounce';
+import throttle from 'lodash-es/throttle';
 
 const breakpoints = Object.entries({ sm: 640, md: 768, lg: 1024, xl: 1280 });
 
@@ -12,17 +12,21 @@ export const ResponsiveMixin = <TBase extends Constructor<LitElement>>(
   return class ResponsiveElement extends BaseElement {
     private __removeBreakpoints?: () => void;
 
-    private __handleResize = debounce((entries: ResizeObserverEntry[]) => {
-      entries.forEach(({ contentRect, target }) => {
-        breakpoints.forEach(([name, minWidth]) => {
-          if (contentRect.width >= minWidth) {
-            if (!target.hasAttribute(name)) target.setAttribute(name, '');
-          } else {
-            if (target.hasAttribute(name)) target.removeAttribute(name);
-          }
+    private __handleResize = throttle(
+      (entries: ResizeObserverEntry[]) => {
+        entries.forEach(({ contentRect, target }) => {
+          breakpoints.forEach(([name, minWidth]) => {
+            if (contentRect.width >= minWidth) {
+              if (!target.hasAttribute(name)) target.setAttribute(name, '');
+            } else {
+              if (target.hasAttribute(name)) target.removeAttribute(name);
+            }
+          });
         });
-      });
-    }, 16);
+      },
+      16,
+      { trailing: true, leading: true }
+    );
 
     connectedCallback(): void {
       super.connectedCallback();
