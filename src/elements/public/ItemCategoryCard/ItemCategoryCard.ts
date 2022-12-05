8abbd0655e40146a2f2960b5065bf6a437e6cc90
Daniil Bratukhin <dantothefuture@gmail.com>
2022-12-05 11:22:15 -0600

    feat(foxy-item-category-card): use one-line layout if there's enough space

--- src/elements/public/ItemCategoryCard/ItemCategoryCard.ts
+++ src/elements/public/ItemCategoryCard/ItemCategoryCard.ts
@@ -1,18 +1,19 @@
 import type { TemplateResult } from 'lit-html';
 import type { Data } from './types';
 
+import { ResponsiveMixin } from '../../../mixins/responsive';
 import { InternalCard } from '../../internal/InternalCard/InternalCard';
 import { html } from 'lit-html';
 
-export class ItemCategoryCard extends InternalCard<Data> {
+export class ItemCategoryCard extends ResponsiveMixin(InternalCard)<Data> {
   renderBody(): TemplateResult {
     const data = this.data;
 
     return html`
-      <div class="flex justify-between gap-s">
+      <div class="leading-s sm-flex sm-justify-between sm-gap-s">
         <div class="font-semibold truncate flex-shrink-0">${data?.name}&ZeroWidthSpace;</div>
         ${data?.name !== data?.code
-          ? html`<div class="truncate text-tertiary">${data?.code}</div>`
+          ? html`<div class="truncate text-tertiary text-s sm-text-m">${data?.code}</div>`
           : ''}
       </div>
     `;
