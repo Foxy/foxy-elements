781113e68e5395c0bfccf5979a6f3087955fa90c
Daniil Bratukhin <dantothefuture@gmail.com>
2022-12-05 11:14:40 -0600

    fix(foxy-item-card): add support for cart and transaction template items where the currency code is inferred

--- src/elements/public/ItemCard/index.ts
+++ src/elements/public/ItemCard/index.ts
@@ -1,5 +1,8 @@
 import '../../internal/InternalCard/index';
+
+import '../NucleonElement/index';
 import '../I18n/index';
+
 import { ItemCard } from './ItemCard';
 
 customElements.define('foxy-item-card', ItemCard);
