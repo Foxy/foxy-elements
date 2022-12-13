2333dd0a210c5e5a2d54e2745703b6c4a9bf1787
Daniil Bratukhin <dantothefuture@gmail.com>
2022-12-12 21:53:20 -0600

    docs(foxy-payments-api-payment-preset-card): add jsdoc

--- src/elements/public/PaymentsApiPaymentPresetCard/PaymentsApiPaymentPresetCard.ts
+++ src/elements/public/PaymentsApiPaymentPresetCard/PaymentsApiPaymentPresetCard.ts
@@ -9,6 +9,23 @@ import { html } from 'lit-html';
 const NS = 'payments-api-payment-preset-card';
 const Base = TranslatableMixin(TwoLineCard, NS);
 
+/**
+ * Card element representing a `fx:payment_preset` resource of Payments API.
+ *
+ * _Payments API is a client-side virtual API layer built on top of hAPI
+ * in an attempt to streamline access to stores' payment method settings
+ * that is currently a bit quirky due to the legacy functionality. To use
+ * this element with hAPI, wrap it into a foxy-payments-api node._
+ *
+ * @slot title:before
+ * @slot title:after
+ *
+ * @slot subtitle:before
+ * @slot subtitle:after
+ *
+ * @element foxy-payments-api-payment-preset-card
+ * @since 1.21.0
+ */
 export class PaymentsApiPaymentPresetCard extends Base<Data> {
   render(): TemplateResult {
     return super.render({
