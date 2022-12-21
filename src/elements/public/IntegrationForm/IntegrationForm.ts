9b4b51b4f94557a067fd6fb9c120e1b1934af327
Daniil Bratukhin <dantothefuture@gmail.com>
2022-12-20 19:20:29 -0600

    refactor(foxy-integration-form): extract repeating markup into renderers

--- src/elements/public/IntegrationForm/IntegrationForm.ts
+++ src/elements/public/IntegrationForm/IntegrationForm.ts
@@ -5,6 +5,7 @@ import type { NucleonV8N } from '../NucleonElement/types';
 
 import { TranslatableMixin } from '../../../mixins/translatable';
 import { InternalForm } from '../../internal/InternalForm/InternalForm';
+import { classMap } from '../../../utils/class-map';
 import { html } from 'lit-html';
 
 const NS = 'integration-form';
@@ -46,17 +47,15 @@ export class IntegrationForm extends Base<Data> {
     const data = this.data as Data;
     const expires = new Date((data.expires ?? 0) * 1000);
     const description = data.project_description?.trim() || this.t('no_description');
+    const postResponse = this.__postResponse;
 
     return html`
-      <dl class="grid grid-cols-1 gap-s">
-        <dt class="sr-only">${this.t('title_description')}</dt>
-        <dd class="font-bold truncate text-xl">${data.project_name}&ZeroWidthSpace;</dd>
+      <div class="space-y-xs">
+        <div class="font-bold truncate text-xl">${data.project_name}&ZeroWidthSpace;</div>
+        <div class="text-secondary">${description}&ZeroWidthSpace;</div>
+      </div>
 
-        <dt class="sr-only">${this.t('subtitle_description')}</dt>
-        <dd class="text-secondary">${description}&ZeroWidthSpace;</dd>
-      </dl>
-
-      ${this.__postResponse
+      ${postResponse
         ? html`
             <div
               class="bg-success-10 text-success p-m pb-s space-y-xs leading-s rounded-t-l rounded-b-l"
@@ -75,140 +74,46 @@ export class IntegrationForm extends Base<Data> {
 
       <table class="font-lumo text-m leading-m w-full">
         <tbody class="divide-y divide-contrast-10">
-          <tr>
-            <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m font-semibold">
-              <foxy-i18n infer="" key="added_by"></foxy-i18n>
-            </td>
-            <td class="max-w-0 truncate py-s text-body w-2-3">
-              ${data.added_by_email
-                ? html`
-                    <a
-                      target="_blank"
-                      class="font-semibold text-primary rounded hover-underline focus-outline-none focus-ring-2 focus-ring-primary-50"
-                      href="mailto:${data.added_by_email}"
-                    >
-                      ${data.added_by_name}
-                    </a>
-                  `
-                : data.added_by_name}
-            </td>
-          </tr>
-
-          <tr>
-            <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m font-semibold">
-              <foxy-i18n infer="" key="contact"></foxy-i18n>
-            </td>
-            <td class="max-w-0 truncate py-s text-body w-2-3">
-              ${data.contact_email
-                ? html`
-                    <a
-                      target="_blank"
-                      class="font-semibold text-primary rounded hover-underline focus-outline-none focus-ring-2 focus-ring-primary-50"
-                      href="mailto:${data.contact_email}"
-                    >
-                      ${data.contact_name}
-                    </a>
-                  `
-                : data.contact_name}
-            </td>
-          </tr>
-
-          <tr>
-            <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m font-semibold">
-              <foxy-i18n infer="" key="company"></foxy-i18n>
-            </td>
-            <td class="max-w-0 truncate py-s text-body w-2-3">
-              ${data.company_url
-                ? html`
-                    <a
-                      target="_blank"
-                      class="font-semibold text-primary rounded hover-underline focus-outline-none focus-ring-2 focus-ring-primary-50"
-                      href=${data.company_url}
-                    >
-                      ${data.company_name}
-                    </a>
-                  `
-                : data.company_name}
-            </td>
-          </tr>
-
-          <tr>
-            <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m font-semibold">
-              <foxy-i18n infer="" key="expires"></foxy-i18n>
-            </td>
-            <td class="max-w-0 truncate py-s text-body w-2-3">
-              <foxy-i18n infer="" key="expires_date" .options=${{ date: expires }}></foxy-i18n>
-            </td>
-          </tr>
-
-          <tr>
-            <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m font-semibold">Client ID</td>
-            <td class="max-w-0 truncate py-s text-body w-2-3">
-              <div class="flex items-center gap-s">
-                <code class="truncate flex-1">${data.client_id}</code>
-                <foxy-copy-to-clipboard
-                  infer="copy-to-clipboard"
-                  class="inline-block"
-                  text=${data.client_id}
-                >
-                </foxy-copy-to-clipboard>
-              </div>
-            </td>
-          </tr>
-
-          ${this.__postResponse
+          ${this.__renderTableRow({
+            label: this.__renderI18n('added_by'),
+            value: this.__renderMailToLink(data.added_by_email, data.added_by_name),
+          })}
+          ${this.__renderTableRow({
+            label: this.__renderI18n('contact'),
+            value: this.__renderMailToLink(data.contact_email, data.contact_name),
+          })}
+          ${this.__renderTableRow({
+            label: this.__renderI18n('company'),
+            value: this.__renderMailToLink(data.company_url, data.company_name),
+          })}
+          ${this.__renderTableRow({
+            label: this.__renderI18n('expires'),
+            value: this.__renderI18n('expires_date', { date: expires }),
+          })}
+          ${this.__renderTableRow({
+            label: 'Client ID',
+            value: this.__renderCopiableText(data.client_id),
+          })}
+          ${postResponse
             ? html`
-                <tr class="font-semibold text-success">
-                  <td class="max-w-0 truncate py-s w-1-3 pr-m font-semibold">Client secret</td>
-                  <td class="max-w-0 py-s w-2-3">
-                    <div class="flex items-center gap-s">
-                      <code class="truncate flex-1">${this.__postResponse.client_secret}</code>
-                      <foxy-copy-to-clipboard
-                        infer="copy-to-clipboard"
-                        class="inline-block"
-                        text=${this.__postResponse.client_secret}
-                      >
-                      </foxy-copy-to-clipboard>
-                    </div>
-                  </td>
-                </tr>
-
-                <tr class="font-semibold text-success">
-                  <td class="max-w-0 truncate py-s w-1-3 pr-m font-semibold">Refresh token</td>
-                  <td class="max-w-0 py-s w-2-3">
-                    <div class="flex items-center gap-s">
-                      <code class="truncate flex-1">${this.__postResponse.refresh_token}</code>
-                      <foxy-copy-to-clipboard
-                        infer="copy-to-clipboard"
-                        class="inline-block"
-                        text=${this.__postResponse.refresh_token}
-                      >
-                      </foxy-copy-to-clipboard>
-                    </div>
-                  </td>
-                </tr>
-
-                <tr class="font-semibold text-success">
-                  <td class="max-w-0 truncate py-s w-1-3 pr-m font-semibold">Access token</td>
-                  <td class="max-w-0 py-s w-2-3">
-                    <div class="flex items-center gap-s">
-                      <code class="truncate flex-1">${this.__postResponse.access_token}</code>
-                      <foxy-copy-to-clipboard
-                        infer="copy-to-clipboard"
-                        class="inline-block"
-                        text=${this.__postResponse.access_token}
-                      >
-                      </foxy-copy-to-clipboard>
-                    </div>
-                  </td>
-                </tr>
+                ${this.__renderTableRow({
+                  highlight: true,
+                  label: 'Client secret',
+                  value: this.__renderCopiableText(postResponse.client_secret),
+                })}
+                ${this.__renderTableRow({
+                  highlight: true,
+                  label: 'Refresh token',
+                  value: this.__renderCopiableText(postResponse.refresh_token),
+                })}
+                ${this.__renderTableRow({
+                  highlight: true,
+                  label: 'Access token',
+                  value: this.__renderCopiableText(postResponse.client_secret),
+                })}
               `
             : ''}
-
-          <tr>
-            <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m font-semibold">Scope</td>
-            <td class="max-w-0 truncate py-s text-body w-2-3"><code>${data.scope}</code></td>
-          </tr>
+          ${this.__renderTableRow({ label: 'Scope', value: html`<code>${data.scope}</code>` })}
         </tbody>
       </table>
 
@@ -224,6 +129,44 @@ export class IntegrationForm extends Base<Data> {
       </foxy-internal-text-area-control>
 
       <foxy-internal-create-control infer="create"></foxy-internal-create-control>
+    `;
+  }
+
+  private __renderTableRow(params: { label: unknown; value: unknown; highlight?: boolean }) {
+    const { label, value, highlight = false } = params;
+    return html`
+      <tr class=${classMap({ 'font-semibold text-success': highlight })}>
+        <td class="max-w-0 truncate py-s text-secondary w-1-3 pr-m">${label}</td>
+        <td class="max-w-0 truncate py-s text-body w-2-3">${value}</td>
+      </tr>
+    `;
+  }
+
+  private __renderMailToLink(email: string, caption: string) {
+    if (!email) return html`${caption}`;
+
+    return html`
+      <a
+        target="_blank"
+        class="font-semibold text-primary rounded hover-underline focus-outline-none focus-ring-2 focus-ring-primary-50"
+        href="mailto:${email}"
+      >
+        ${caption}
+      </a>
+    `;
+  }
+
+  private __renderI18n(key: string, options?: unknown) {
+    return html`<foxy-i18n infer="" key=${key} .options=${options}></foxy-i18n>`;
+  }
+
+  private __renderCopiableText(text: string) {
+    return html`
+      <div class="flex items-center gap-s">
+        <code class="truncate flex-1">${text}</code>
+        <foxy-copy-to-clipboard infer="copy-to-clipboard" class="inline-block" text=${text}>
+        </foxy-copy-to-clipboard>
+      </div>
     `;
   }
 }
