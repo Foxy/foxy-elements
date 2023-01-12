2f6527418564058c4e105061cc2d005c393fdbab
Daniil Bratukhin <dantothefuture@gmail.com>
2023-01-11 21:21:08 -0600

    refactor(foxy-payments-api-payment-preset-form): use built-in labels for lists

--- src/elements/public/PaymentsApiPaymentPresetForm/PaymentsApiPaymentPresetForm.ts
+++ src/elements/public/PaymentsApiPaymentPresetForm/PaymentsApiPaymentPresetForm.ts
@@ -2,9 +2,9 @@ import type { InternalCheckboxGroupControl } from '../../internal/InternalCheckb
 import type { PropertyDeclarations } from 'lit-element';
 import type { FormRendererContext } from '../FormDialog/types';
 import type { ItemRendererContext } from '../CollectionPage/types';
+import type { Data, Templates } from './types';
 import type { TemplateResult } from 'lit-html';
 import type { NucleonV8N } from '../NucleonElement/types';
-import type { Data } from './types';
 
 import { TranslatableMixin } from '../../../mixins/translatable';
 import { InternalForm } from '../../internal/InternalForm/InternalForm';
@@ -13,6 +13,41 @@ import { html } from 'lit-html';
 const NS = 'payments-api-payment-preset-form';
 const Base = TranslatableMixin(InternalForm, NS);
 
+/**
+ * Form element for the `fx:payment_preset` resource of Payments API.
+ *
+ * _Payments API is a client-side virtual API layer built on top of hAPI
+ * in an attempt to streamline access to stores' payment method settings
+ * that is currently a bit quirky due to the legacy functionality. To use
+ * this element with hAPI, wrap it into a foxy-payments-api node._
+ *
+ * @slot description:before
+ * @slot description:after
+ *
+ * @slot is-live:before
+ * @slot is-live:after
+ *
+ * @slot is-purchase-order-enabled:before
+ * @slot is-purchase-order-enabled:after
+ *
+ * @slot payment-methods:before
+ * @slot payment-methods:after
+ *
+ * @slot fraud-protections:before
+ * @slot fraud-protections:after
+ *
+ * @slot timestamps:before
+ * @slot timestamps:after
+ *
+ * @slot create:before
+ * @slot create:after
+ *
+ * @slot delete:before
+ * @slot delete:after
+ *
+ * @element foxy-payments-api-payment-preset-form
+ * @since 1.21.0
+ */
 export class PaymentsApiPaymentPresetForm extends Base<Data> {
   static get properties(): PropertyDeclarations {
     return {
@@ -29,9 +64,14 @@ export class PaymentsApiPaymentPresetForm extends Base<Data> {
     ];
   }
 
+  /** A function that returns image URL for given fraud protection `type`. */
   getFraudProtectionImageSrc: ((type: string) => string) | null = null;
 
+  /** A function that returns image URL for given payment method `type`. */
   getPaymentMethodImageSrc: ((type: string) => string) | null = null;
+
+  /** Template render functions mapped to their name. */
+  templates: Templates = {};
 
   private static readonly __isPOEnabledOptions = [{ label: 'option_true', value: 'true' }];
 
@@ -79,81 +119,61 @@ export class PaymentsApiPaymentPresetForm extends Base<Data> {
 
       ${this.data
         ? html`
-            <div class="grid grid-cols-1 gap-s">
-              <foxy-i18n
-                class="text-s font-medium text-secondary"
-                infer="payment-methods"
-                key="title"
-              >
-              </foxy-i18n>
+            <foxy-internal-async-list-control
+              infer="payment-methods"
+              first=${this.data._links['fx:payment_methods'].href}
+              limit="5"
+              .item=${(ctx: ItemRendererContext) => html`
+                <foxy-payments-api-payment-method-card
+                  parent=${ctx.parent}
+                  infer="payments-api-payment-method-card"
+                  href=${ctx.href}
+                  .getImageSrc=${this.getPaymentMethodImageSrc}
+                >
+                </foxy-payments-api-payment-method-card>
+              `}
+              .form=${(ctx: FormRendererContext) => html`
+                <foxy-payments-api-payment-method-form
+                  parent=${ctx.dialog.parent}
+                  infer="payments-api-payment-method-form"
+                  href=${ctx.dialog.href}
+                  id="form"
+                  .getImageSrc=${this.getPaymentMethodImageSrc}
+                  @fetch=${ctx.handleFetch}
+                  @update=${ctx.handleUpdate}
+                >
+                </foxy-payments-api-payment-method-form>
+              `}
+            >
+            </foxy-internal-async-list-control>
 
-              <foxy-internal-async-list-control
-                infer="payment-methods"
-                first=${this.data._links['fx:payment_methods'].href}
-                limit="5"
-                .item=${(ctx: ItemRendererContext) => html`
-                  <foxy-payments-api-payment-method-card
-                    parent=${ctx.parent}
-                    infer="payments-api-payment-method-card"
-                    class="px-m py-s"
-                    href=${ctx.href}
-                    .getImageSrc=${this.getPaymentMethodImageSrc}
-                  >
-                  </foxy-payments-api-payment-method-card>
-                `}
-                .form=${(ctx: FormRendererContext) => html`
-                  <foxy-payments-api-payment-method-form
-                    parent=${ctx.dialog.parent}
-                    infer="payments-api-payment-method-form"
-                    href=${ctx.dialog.href}
-                    id="form"
-                    .getImageSrc=${this.getPaymentMethodImageSrc}
-                    @fetch=${ctx.handleFetch}
-                    @update=${ctx.handleUpdate}
-                  >
-                  </foxy-payments-api-payment-method-form>
-                `}
-              >
-              </foxy-internal-async-list-control>
-            </div>
-
-            <div class="grid grid-cols-1 gap-s">
-              <foxy-i18n
-                class="text-s font-medium text-secondary"
-                infer="fraud-protections"
-                key="title"
-              >
-              </foxy-i18n>
-
-              <foxy-internal-async-list-control
-                infer="fraud-protections"
-                first=${this.data._links['fx:fraud_protections'].href}
-                limit="5"
-                .item=${(ctx: ItemRendererContext) => html`
-                  <foxy-payments-api-fraud-protection-card
-                    parent=${ctx.parent}
-                    infer="payments-api-fraud-protection-card"
-                    class="px-m py-s"
-                    href=${ctx.href}
-                    .getImageSrc=${this.getFraudProtectionImageSrc}
-                  >
-                  </foxy-payments-api-fraud-protection-card>
-                `}
-                .form=${(ctx: FormRendererContext) => html`
-                  <foxy-payments-api-fraud-protection-form
-                    parent=${ctx.dialog.parent}
-                    infer="payments-api-fraud-protection-form"
-                    href=${ctx.dialog.href}
-                    id="form"
-                    .getImageSrc=${this.getFraudProtectionImageSrc}
-                    @fetch=${ctx.handleFetch}
-                    @update=${ctx.handleUpdate}
-                  >
-                  </foxy-payments-api-fraud-protection-form>
-                `}
-              >
-              </foxy-internal-async-list-control>
-            </div>
+            <foxy-internal-async-list-control
+              infer="fraud-protections"
+              first=${this.data._links['fx:fraud_protections'].href}
+              limit="5"
+              .item=${(ctx: ItemRendererContext) => html`
+                <foxy-payments-api-fraud-protection-card
+                  parent=${ctx.parent}
+                  infer="payments-api-fraud-protection-card"
+                  href=${ctx.href}
+                  .getImageSrc=${this.getFraudProtectionImageSrc}
+                >
+                </foxy-payments-api-fraud-protection-card>
+              `}
+              .form=${(ctx: FormRendererContext) => html`
+                <foxy-payments-api-fraud-protection-form
+                  parent=${ctx.dialog.parent}
+                  infer="payments-api-fraud-protection-form"
+                  href=${ctx.dialog.href}
+                  id="form"
+                  .getImageSrc=${this.getFraudProtectionImageSrc}
+                  @fetch=${ctx.handleFetch}
+                  @update=${ctx.handleUpdate}
+                >
+                </foxy-payments-api-fraud-protection-form>
+              `}
+            >
+            </foxy-internal-async-list-control>
           `
         : ''}
       ${super.renderBody()}
