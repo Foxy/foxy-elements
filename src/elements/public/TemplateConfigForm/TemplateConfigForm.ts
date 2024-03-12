bfe2e0ed9bf36f023f598bb9874a94ef8a27e946
Daniil Bratukhin <dantothefuture@gmail.com>
2024-03-12 12:49:50 -0300

    feat(foxy-template-config-form): hide deprecated google analytics config by default

--- src/elements/public/TemplateConfigForm/TemplateConfigForm.ts
+++ src/elements/public/TemplateConfigForm/TemplateConfigForm.ts
@@ -19,6 +19,7 @@ import { TranslatableMixin } from '../../../mixins/translatable';
 import { classMap } from '../../../utils/class-map';
 import { getDefaultJSON } from './defaults';
 import { live } from 'lit-html/directives/live';
+import { BooleanSelector } from '@foxy.io/sdk/core';
 
 const NS = 'template-config-form';
 const Base = ScopedElementsMixin(
@@ -212,6 +213,17 @@ export class TemplateConfigForm extends Base<Data> {
         </div>
       </div>
     `;
+  }
+
+  get hiddenSelector(): BooleanSelector {
+    const alwaysHidden = [super.hiddenSelector.toString()];
+    const json: TemplateConfigJSON = this.data?.json ? JSON.parse(this.data.json) : null;
+
+    if (!json || json.analytics_config.google_analytics.usage === 'none') {
+      alwaysHidden.push('google-analytics');
+    }
+
+    return new BooleanSelector(alwaysHidden.join(' ').trim());
   }
 
   private __renderCartType(json: TemplateConfigJSON) {
