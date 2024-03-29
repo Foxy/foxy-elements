d1236f5d0a4e9e4fcc64a0171bf8866691c4056f
Daniil Bratukhin <dantothefuture@gmail.com>
2024-03-29 17:10:57 -0300

    feat(foxy-subscription-form): replace End Subscription dialog with a date picker

--- src/elements/public/SubscriptionForm/index.ts
+++ src/elements/public/SubscriptionForm/index.ts
@@ -11,29 +11,15 @@ import '../../internal/InternalCalendar/index';
 import '../../internal/InternalSandbox/index';
 import '../../internal/InternalForm/index';
 
-import '../CancellationForm/index';
 import '../TransactionCard/index';
 import '../NucleonElement/index';
 import '../AttributeCard/index';
 import '../AttributeForm/index';
 import '../CustomerCard/index';
-import '../FormDialog/index';
 import '../ItemCard/index';
 import '../I18n/index';
 
-import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles';
-
 import { SubscriptionForm } from './SubscriptionForm';
-
-/** https://github.com/vaadin-component-factory/vcf-tooltip/issues/6 */
-registerStyles(
-  'vcf-tooltip',
-  css`
-    :host([hidden]) [part='container'] {
-      display: none !important;
-    }
-  `
-);
 
 customElements.define('foxy-subscription-form', SubscriptionForm);
 
