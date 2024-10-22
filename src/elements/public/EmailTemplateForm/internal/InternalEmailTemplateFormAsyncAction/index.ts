cc0d0bf93f0011b6440e3c7831859ddb1a90a466
Daniil Bratukhin <dantothefuture@gmail.com>
2024-10-22 19:43:18 -0300

    feat(foxy-email-template-form): rebuild with updated controls

---
+++ src/elements/public/EmailTemplateForm/internal/InternalEmailTemplateFormAsyncAction/index.ts
@@ -0,0 +1,9 @@
+import '@vaadin/vaadin-button';
+import '../../../../internal/InternalControl/index';
+import '../../../I18n/index';
+
+import { InternalEmailTemplateFormAsyncAction as Control } from './InternalEmailTemplateFormAsyncAction';
+
+customElements.define('foxy-internal-email-template-form-async-action', Control);
+
+export { Control as InternalEmailTemplateFormAsyncAction };
