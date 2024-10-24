fe7d9f2b83f26384e34933e5bbcc8d1681bd43a9
Daniil Bratukhin <dantothefuture@gmail.com>
2024-10-24 17:00:09 -0300

    internal(editable-control): add support for async v8n to base class

--- src/elements/internal/InternalEditableControl/InternalEditableControl.ts
+++ src/elements/internal/InternalEditableControl/InternalEditableControl.ts
@@ -1,5 +1,8 @@
 import type { PropertyDeclarations } from 'lit-element';
+
 import { InternalControl } from '../InternalControl/InternalControl';
+
+import debounce from 'lodash-es/debounce';
 
 /**
  * An internal base class for controls that have editing functionality, e.g. a text field.
@@ -14,6 +17,7 @@ export class InternalEditableControl extends InternalControl {
   static get properties(): PropertyDeclarations {
     return {
       ...super.properties,
+      checkValidityAsync: { attribute: false },
       placeholder: { type: String, noAccessor: true },
       helperText: { type: String, attribute: 'helper-text', noAccessor: true },
       v8nPrefix: { type: String, attribute: 'v8n-prefix', noAccessor: true },
@@ -21,16 +25,29 @@ export class InternalEditableControl extends InternalControl {
       setValue: { attribute: false },
       property: { type: String, noAccessor: true },
       label: { type: String, noAccessor: true },
+      __asyncError: { attribute: false },
     };
   }
+
+  checkValidityAsync: ((value: unknown) => Promise<true | string>) | null = null;
 
   getValue = (): unknown => this.nucleon?.form[this.property];
 
   setValue = (newValue: unknown): void => this.nucleon?.edit({ [this.property]: newValue });
 
+  private __debouncedCheckValidityAsync = debounce(async (newValue: unknown) => {
+    const validOrError = await this.checkValidityAsync?.(newValue);
+    if (this._value !== newValue) return;
+    this.__asyncError = validOrError === true ? null : validOrError ?? null;
+  }, 300);
+
+  private __previousValue: unknown | null = null;
+
   private __placeholder: string | null = null;
 
   private __helperText: string | null = null;
+
+  private __asyncError: string | null = null;
 
   private __v8nPrefix: string | null = null;
 
@@ -169,28 +186,34 @@ export class InternalEditableControl extends InternalControl {
    * Assigning a value to this property will dispatch a cancelable `change` event
    * with the new value in the detail and write changes to the NucleonElement instance if permitted.
    */
-
   protected get _value(): unknown | undefined {
     return this.getValue();
   }
 
   protected set _value(newValue: unknown | undefined) {
+    this.__previousValue = this._value;
+    this.__asyncError = null;
+    if (!this._error && this.__previousValue !== newValue) {
+      this.__debouncedCheckValidityAsync(newValue);
+    }
+
     const event = new CustomEvent('change', { cancelable: true, detail: newValue });
     const useDefaultAction = this.dispatchEvent(event);
     if (useDefaultAction) this.setValue(newValue);
   }
+
   /** A shortcut returning the first v8n error associated with this control. */
-
   protected get _error(): string | undefined {
-    return this.nucleon?.errors.find(v => v.startsWith(this.v8nPrefix));
+    const syncError = this.nucleon?.errors.find(v => v.startsWith(this.v8nPrefix));
+    return syncError ?? this.__asyncError ?? void 0;
   }
-  /** A shortcut returning the localized text of the first v8n error associated with this control. */
 
+  /** A shortcut returning the localized text of the first v8n error associated with this control. */
   protected get _errorMessage(): string | undefined {
     return this._error ? this.t(this._error.substring(this.v8nPrefix.length)) : undefined;
   }
-  /** A helper returning a `.checkValidity()` function for use with Vaadin elements. */
 
+  /** A helper returning a `.checkValidity()` function for use with Vaadin elements. */
   protected get _checkValidity(): () => boolean {
     return () => !this._error;
   }
