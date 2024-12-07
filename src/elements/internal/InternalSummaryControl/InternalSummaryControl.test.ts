9d0a9476a1d0e199b364c19222b23a7761816bae
Daniil Bratukhin <dantothefuture@gmail.com>
2024-12-07 03:23:58 -0300

    internal(summary-control): add details/summary layout

--- src/elements/internal/InternalSummaryControl/InternalSummaryControl.test.ts
+++ src/elements/internal/InternalSummaryControl/InternalSummaryControl.test.ts
@@ -20,11 +20,21 @@ describe('InternalSummaryControl', () => {
     expect(new Control()).to.be.instanceOf(customElements.get('foxy-internal-editable-control'));
   });
 
+  it('has a reactive property "layout" that defaults to null', () => {
+    expect(Control).to.have.deep.nested.property('properties.layout', {});
+    expect(new Control()).to.have.property('layout', null);
+  });
+
+  it('has a reactive property "open" that defaults to false', () => {
+    expect(Control).to.have.deep.nested.property('properties.open', { type: Boolean });
+    expect(new Control()).to.have.property('open', false);
+  });
+
   it('renders nothing in light DOM', () => {
     expect(new Control().renderLightDom()).to.be.undefined;
   });
 
-  it('renders label', async () => {
+  it('renders label in default layout', async () => {
     const control = await fixture<Control>(html`
       <foxy-internal-summary-control></foxy-internal-summary-control>
     `);
@@ -38,7 +48,7 @@ describe('InternalSummaryControl', () => {
     expect(control.renderRoot).to.include.text('Foo bar');
   });
 
-  it('renders helper text', async () => {
+  it('renders helper text in default layout', async () => {
     const control = await fixture<Control>(html`
       <foxy-internal-summary-control></foxy-internal-summary-control>
     `);
@@ -52,11 +62,100 @@ describe('InternalSummaryControl', () => {
     expect(control.renderRoot).to.include.text('Test helper text');
   });
 
-  it('renders default slot', async () => {
+  it('renders default slot in default layout', async () => {
     const control = await fixture<Control>(html`
       <foxy-internal-summary-control></foxy-internal-summary-control>
     `);
 
     expect(control.renderRoot).to.include.html('<slot></slot>');
+  });
+
+  it('renders details/summary in "details" layout', async () => {
+    const control = await fixture<Control>(html`
+      <foxy-internal-summary-control layout="details"></foxy-internal-summary-control>
+    `);
+
+    const details = control.renderRoot.querySelector('details')!;
+    const summary = control.renderRoot.querySelector('details > summary')!;
+
+    expect(details).to.exist;
+    expect(summary).to.exist;
+    expect(details.open).to.be.false;
+
+    control.open = true;
+    await control.requestUpdate();
+    expect(details.open).to.be.true;
+  });
+
+  it('renders label inside of the details summary in details layout', async () => {
+    const control = await fixture<Control>(html`
+      <foxy-internal-summary-control layout="details"></foxy-internal-summary-control>
+    `);
+
+    const summary = control.renderRoot.querySelector('summary');
+    expect(summary).to.include.text('label');
+
+    control.label = 'Foo bar';
+    await control.requestUpdate();
+
+    expect(summary).to.not.include.text('label');
+    expect(summary).to.include.text('Foo bar');
+  });
+
+  it('renders helper text inside of the details summary in details layout', async () => {
+    const control = await fixture<Control>(html`
+      <foxy-internal-summary-control layout="details"></foxy-internal-summary-control>
+    `);
+
+    const summary = control.renderRoot.querySelector('summary');
+    expect(summary).to.include.text('helper_text');
+
+    control.helperText = 'Test helper text';
+    await control.requestUpdate();
+
+    expect(summary).to.not.include.text('helper_text');
+    expect(summary).to.include.text('Test helper text');
+  });
+
+  it('renders default slot inside of the details content in details layout', async () => {
+    const control = await fixture<Control>(html`
+      <foxy-internal-summary-control layout="details"></foxy-internal-summary-control>
+    `);
+
+    const summary = control.renderRoot.querySelector('details');
+    expect(summary).to.include.html('<slot></slot>');
+  });
+
+  it('toggles open state when details is toggled', async () => {
+    const control = await fixture<Control>(html`
+      <foxy-internal-summary-control layout="details"></foxy-internal-summary-control>
+    `);
+
+    const details = control.renderRoot.querySelector('details')!;
+    expect(control.open).to.be.false;
+
+    details.open = true;
+    details.dispatchEvent(new Event('toggle'));
+    await control.requestUpdate();
+    expect(control.open).to.be.true;
+
+    details.open = false;
+    details.dispatchEvent(new Event('toggle'));
+    await control.requestUpdate();
+    expect(control.open).to.be.false;
+  });
+
+  it('dispatches "toggle" event when details is toggled', async () => {
+    const control = await fixture<Control>(html`
+      <foxy-internal-summary-control layout="details"></foxy-internal-summary-control>
+    `);
+
+    const details = control.renderRoot.querySelector('details')!;
+    let eventCount = 0;
+
+    control.addEventListener('toggle', () => eventCount++);
+    details.dispatchEvent(new Event('toggle'));
+    await control.requestUpdate();
+    expect(eventCount).to.equal(1);
   });
 });
--- src/elements/internal/InternalSummaryControl/InternalSummaryControl.ts
+++ src/elements/internal/InternalSummaryControl/InternalSummaryControl.ts
@@ -1,10 +1,18 @@
-import type { CSSResultArray } from 'lit-element';
-import type { TemplateResult } from 'lit-html';
+import type { CSSResultArray, PropertyDeclarations } from 'lit-element';
+import { svg, TemplateResult } from 'lit-html';
 
 import { InternalEditableControl } from '../InternalEditableControl/InternalEditableControl';
 import { html, css } from 'lit-element';
 
 export class InternalSummaryControl extends InternalEditableControl {
+  static get properties(): PropertyDeclarations {
+    return {
+      ...super.properties,
+      layout: {},
+      open: { type: Boolean },
+    };
+  }
+
   static get styles(): CSSResultArray {
     return [
       ...super.styles,
@@ -13,15 +21,66 @@ export class InternalSummaryControl extends InternalEditableControl {
           background-color: var(--lumo-contrast-5pct);
           padding: calc(0.625em + (var(--lumo-border-radius) / 4) - 1px);
         }
+
+        details summary > div {
+          border-radius: var(--lumo-border-radius);
+        }
+
+        details[open] summary > div {
+          border-radius: var(--lumo-border-radius) var(--lumo-border-radius) 0 0;
+        }
       `,
     ];
   }
+
+  layout: null | 'details' = null;
+
+  open = false;
 
   renderLightDom(): void {
     return;
   }
 
   renderControl(): TemplateResult {
+    if (this.layout === 'details') {
+      return html`
+        <details
+          class="rounded overflow-hidden"
+          ?open=${this.open}
+          @toggle=${(evt: Event) => {
+            const details = evt.currentTarget as HTMLDetailsElement;
+            this.open = details.open;
+            if (!evt.composed) this.dispatchEvent(new CustomEvent('toggle'));
+          }}
+        >
+          <summary class="select-none cursor-pointer focus-outline-none group">
+            <div
+              class="leading-s bg-contrast-5 group-focus-ring-2 group-focus-ring-inset group-focus-ring-primary-50"
+              style="padding: calc(0.625em + (var(--lumo-border-radius) / 4) - 1px)"
+              ?hidden=${!this.label && !this.helperText}
+            >
+              <p
+                class="font-medium text-body text-l flex items-center justify-between gap-s"
+                ?hidden=${!this.label}
+              >
+                <span>${this.label}</span>
+                <span
+                  class="flex items-center justify-center transition-colors text-tertiary group-hover-text-body"
+                  style="transform: translateX(0.2em)"
+                >
+                  ${svg`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" style="width: 1.2em; height: 1.2em"><path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" /></svg>`}
+                </span>
+              </p>
+              <p class="text-s text-secondary" ?hidden=${!this.helperText}>${this.helperText}</p>
+            </div>
+          </summary>
+          <div class="overflow-hidden grid" style="gap: 1px">
+            <slot></slot>
+          </div>
+        </details>
+      `;
+    }
+
     return html`
       <div class="leading-s mb-s" ?hidden=${!this.label && !this.helperText}>
         <p class="font-medium text-body text-l" ?hidden=${!this.label}>${this.label}</p>
