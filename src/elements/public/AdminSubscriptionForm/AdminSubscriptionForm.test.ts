4e957d3e8c2bd4ded494d32a26b0b5dc36fffe1e
Daniil Bratukhin <dantothefuture@gmail.com>
2025-01-20 14:35:26 -0300

    fix(foxy-admin-subscription-form): revert to iso-long format in start/next/end dates

--- src/elements/public/AdminSubscriptionForm/AdminSubscriptionForm.test.ts
+++ src/elements/public/AdminSubscriptionForm/AdminSubscriptionForm.test.ts
@@ -193,6 +193,7 @@ describe('AdminSubscriptionForm', () => {
 
     expect(control?.localName).to.equal('foxy-internal-date-control');
     expect(control).to.have.attribute('layout', 'summary-item');
+    expect(control).to.have.attribute('format', 'iso-long');
   });
 
   it('renders frequency control inside of the general summary control', async () => {
@@ -232,6 +233,7 @@ describe('AdminSubscriptionForm', () => {
 
     expect(control?.localName).to.equal('foxy-internal-date-control');
     expect(control).to.have.attribute('layout', 'summary-item');
+    expect(control).to.have.attribute('format', 'iso-long');
   });
 
   it('renders date control for end date inside of the general summary control', async () => {
@@ -251,6 +253,7 @@ describe('AdminSubscriptionForm', () => {
 
     expect(control?.localName).to.equal('foxy-internal-date-control');
     expect(control).to.have.attribute('layout', 'summary-item');
+    expect(control).to.have.attribute('format', 'iso-long');
   });
 
   it('renders summary control with overdue information', async () => {
