aa86254b8ba4259fc98cbbd894407dce897edf75
Daniil Bratukhin <dantothefuture@gmail.com>
2025-04-24 20:03:02 -0300

    refactor: update styles of summary headers

--- src/elements/internal/InternalSummaryControl/InternalSummaryControl.ts
+++ src/elements/internal/InternalSummaryControl/InternalSummaryControl.ts
@@ -85,14 +85,14 @@ export class InternalSummaryControl extends InternalEditableControl {
               style="padding: calc(0.625em + (var(--lumo-border-radius) / 4) - 1px)"
               ?hidden=${!this.label && !this.helperText}
             >
-              <p class="flex items-center justify-between gap-s" ?hidden=${!this.label}>
-                <span>
+              <p
+                class="flex items-center justify-between gap-s transition-opacity group-hover-opacity-80"
+                ?hidden=${!this.label}
+              >
+                <span class="font-medium">
                   ${this.label}${typeof this.count === 'number' ? ` (${this.count})` : ''}
                 </span>
-                <span
-                  class="flex items-center justify-center transition-colors text-tertiary group-hover-text-body"
-                  style="transform: scale(1.35)"
-                >
+                <span class="flex items-center justify-center" style="transform: scale(1.35)">
                   ${svg`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" style="width: 1.2em; height: 1.2em"><path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" /></svg>`}
                 </span>
               </p>
