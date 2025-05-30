e7bb78483faadce2c3deea48e9c4882de87dfa1c
Daniil Bratukhin <dantothefuture@gmail.com>
2025-05-29 21:32:11 -0300

    fix: fix unstable month name generation in date pickers

--- src/elements/internal/InternalDateControl/getMonthNames.ts
+++ src/elements/internal/InternalDateControl/getMonthNames.ts
@@ -1,6 +1,6 @@
 export function getMonthNames(lang: string): string[] {
-  return new Array(12).fill(new Date()).map((date: Date, i) => {
-    date.setMonth(i);
+  return new Array(12).fill(null).map((_, i) => {
+    const date = new Date(2000, i, 1);
     return date.toLocaleDateString(lang, { month: 'long' });
   });
 }
